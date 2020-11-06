f8be1dea5262e4d1ee7704fd34165141463f9abe
Gonzalo Carrillo <mynameisgonz@gmail.com>
2020-11-06 12:10:54 -0500

    Create Gather-Timezone-Offset-DST.ps1

---
+++ Scripts/Powershell/Gather-Timezone-Offset-DST.ps1
@@ -0,0 +1,19 @@
+﻿## Kaseya Automation Team
+## Used by the "Gather Timezone Offset and DST settings" Agent Procedure
+param (
+    [parameter(Mandatory=$true)]
+    [string]$AgentName = "",
+    [parameter(Mandatory=$true)]
+	[string]$Path = ""
+)
+
+$UTCOffset = Get-TimeZone|Select-Object -ExpandProperty BaseUTCOffset
+
+$DSTstatus = Get-TimeZone|Select-Object -ExpandProperty SupportsDaylightSavingTime
+
+$Output = New-Object psobject
+Add-Member -InputObject $Output -MemberType NoteProperty -Name MachineID -Value $AgentName
+Add-Member -InputObject $Output -MemberType NoteProperty -Name Offset -Value $UTCOffset
+Add-Member -InputObject $Output -MemberType NoteProperty -Name DST -Value $DSTstatus
+
+Export-Csv -InputObject $Output -Path $Path -NoTypeInformation

4e6992c72f51fb7c0463ceab1eb0c889eddc5650
Gonzalo Carrillo <gonzalo.carrillo@kaseya.com>
2020-11-10 09:55:31 -0500

    upload of new file

---
+++ Scripts/Powershell/Get-File-Size.ps1
@@ -0,0 +1,3 @@
+﻿$folder = $args[0]
+$sz = $args[1]
+"{0}" -f [math]::Round(((Get-ChildItem $folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / $sz),2)
---
+++ Scripts/Powershell/Get-Last-Modified-Date.ps1
@@ -0,0 +1,15 @@
+﻿$targetff = $args[0]
+$daysold = $args[1]
+$lastDay = (Get-Date).AddDays(-$daysold)
+#while ($true)
+#{
+    if ((Get-ItemProperty -Path $targetff -Name LastWriteTime).LastWriteTime -gt $lastDay)
+    {
+        write-host "Folder has been Updated within the $daysold day(s)"
+    }
+    else
+    {
+        write-host "Folder has not been Updated more than $daysold day(s), since"(Get-ItemProperty -Path $targetff -Name LastWriteTime).LastWriteTime
+    }
+    # start-sleep -seconds 300
+#}

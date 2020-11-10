35860cbcff866c2b7c6b75d8cdf311a38f1525a9
Gonzalo Carrillo <gonzalo.carrillo@kaseya.com>
2020-11-10 10:56:05 -0500

    test

--- Scripts/Powershell/Get-File-Size.ps1
+++ Scripts/Powershell/Get-File-Size.ps1
@@ -1,7 +1,7 @@
 ﻿$folder = $args[0]
 $sz = $args[1]
 if ($sz.ToLower() -eq "gb") {
-    "{0}" -f [math]::Round(((Get-ChildItem $folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / GB),2)
+    "{0}" -f [math]::Round(((Get-ChildItem $folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1GB),2)
 } else {
-	"{0}" -f [math]::Round(((Get-ChildItem $folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / MB),2)
+	"{0}" -f [math]::Round(((Get-ChildItem $folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB),2)
 }

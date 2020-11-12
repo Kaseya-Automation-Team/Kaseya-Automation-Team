0e789d028e6f3c5ed82012aac6de460a4cdee003
Gonzalo Carrillo <gonzalo.carrillo@kaseya.com>
2020-11-12 17:55:37 -0500

    uploaded new ap

---
+++ Scripts/Powershell/Gather-LogEvents.ps1
@@ -0,0 +1,71 @@
+﻿param (
+    [parameter(Mandatory=$true)]
+    [string]$AgentName = "",
+    [parameter(Mandatory=$true)]
+    [string]$FileName = "",
+    [parameter(Mandatory=$true)]
+    [string]$Path = "",
+    [parameter(Mandatory=$true)]
+    [string]$LogName = "",
+    [parameter(Mandatory=$true)]
+    [int]$Days = ""
+ )
+
+<#
+.Synopsis
+   Saves all events with specified level from specified Windows log. By default it runs for 1 (Critical) and 2 (Error).
+.DESCRIPTION
+   Using Get-WinEvent command script queries specified system log (System for example) for specified error levels (1 - Critical, 2 - Error, for example)
+.EXAMPLE
+   .\Get-LogEvents.ps1 -AgentName "432423422" -Path "c:\kworking" -Filename "failed_logins.csv" -LogName "System" -Days 30
+.NOTES
+   Version 0.1
+   Author: Aliaksandr Serzhankou
+   Email: a.serzhankou@kaseya.com
+#>
+
+#Create array where all objects for export will be storred
+$Results = @()
+
+#Convert days to the specic date
+$StartTime = (Get-date).AddDays(-$Days)
+
+$AllEvents = Get-WinEvent -FilterHashtable @{
+    StartTime=$StartTime
+    Logname=$LogName
+    #1 is Critical, 2 is Error
+    Level=1, 2
+}
+
+ForEach ($Event in $AllEvents) {
+
+    #Create new obect
+    $Output = New-Object psobject
+
+    #Collect time and date
+    $Time = $Event.TimeCreated
+    $Time = Get-Date $Time -Format 'dd/MM/yy hh:mm:ss:ms'
+    $Time = $Time -replace "-", "/"
+
+    #Collect level id and level name
+    $LevelName = $Event.LevelDisplayName
+    $Level = $Event.Level
+
+    #Collect message
+    $Message = $Event.Message
+
+    #Fill newly created objects with data
+    Add-Member -InputObject $Output -MemberType NoteProperty -Name AgentGuid -Value $AgentName
+    Add-Member -InputObject $Output -TypeName LevelName -MemberType NoteProperty -Name LevelName -Value $LevelName
+    Add-Member -InputObject $Output -TypeName Level -MemberType NoteProperty -Name Level -Value $Level
+    Add-Member -InputObject $Output -TypeName Message -MemberType NoteProperty -Name Message -Value $Message
+    Add-Member -InputObject $Output -TypeName DateTime -MemberType NoteProperty -Name DateTime -Value $Time
+
+
+    #Add object to the previously created array
+    $Results += $Output
+
+}
+
+#Export results to csv file
+$Results| Export-Csv -Path $Path\$FileName -NoTypeInformation -Encoding UTF8

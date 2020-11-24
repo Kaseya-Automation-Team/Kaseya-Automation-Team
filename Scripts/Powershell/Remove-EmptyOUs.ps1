b28d67f770b10d5a3b3e817431f36919a4327e0e
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2020-11-24 18:56:55 +0300

    Add files via upload

---
+++ Scripts/Powershell/Remove-EmptyOUs.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.Synopsis
+   Finds and removes empty OUs
+.DESCRIPTION
+   Finds empty OUs in AD and removes them
+.EXAMPLE
+   Remove-EmptyOUs
+.NOTES
+   Version 0.1
+   Author: Vladislav Semko
+   Email: Vladislav.Semko@kaseya.com
+#>
+
+Import-Module ActiveDirectory
+
+Get-ADOrganizationalUnit -Filter * | ForEach-Object {
+	   if (-not (Get-ADObject -SearchBase $_ -SearchScope OneLevel -Filter * )) {
+      		Write-Output $_
+   	}
+} | Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false -PassThru | Remove-ADOrganizationalUnit -Confirm:$false

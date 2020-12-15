536f2238d652bb1ff58cbb071e3c8f692cc22c04
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2020-12-15 14:26:17 +0300

    Gather Account Lockout Threshold value
    Script gathers value of Account Lockout Threshold from the policies and exports it to CSV file

---
+++ Scripts/Powershell/Gather-LockoutThreshold.ps1
@@ -0,0 +1,29 @@
+﻿## Kaseya Automation Team
+## Used by the "Gather Account Lockout Threshold" Agent Procedure
+param (
+    [parameter(Mandatory=$true)]
+    [string]$AgentName = "",
+    [parameter(Mandatory=$true)]
+	[string]$Path = ""
+)
+
+
+$Output = New-Object psobject
+
+Add-Member -InputObject $Output -MemberType NoteProperty -Name MachineID -Value $AgentName
+
+#Store path to temp directory into custom variable
+$Temp = $env:TEMP
+
+#If computer is NOT part of domain, export current security policies into file
+secedit.exe /export /cfg $Temp\secpol.cfg /quiet
+
+#Gather value of account lockout threshold
+$LockoutThreshold = Get-Content $Temp\secpol.cfg | Select-String -Pattern "LockoutBadCount = [0-9]+"
+$LockoutThreshold =  $LockoutThreshold.Line.Split(" = ")[3]
+
+#Clean up
+Remove-Item -force $Temp\secpol.cfg -confirm:$false
+Add-Member -InputObject $Output -MemberType NoteProperty -Name AccountLockoutThreshold -Value $LockoutThreshold
+
+Export-Csv -InputObject $Output -Path $Path -NoTypeInformation

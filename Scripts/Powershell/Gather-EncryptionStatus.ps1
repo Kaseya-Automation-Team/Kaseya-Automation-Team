4f373593faa93cb0a8e893c7600b9a637646fcf6
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2020-12-16 13:31:46 +0300

    Gather BitLocker statuses
    Checks if BitLocker protections is enabled on hard disk, if it's enable for removable devices and also checks if legal notice message is configured.
    
    All gathered information exports to CSV file.

---
+++ Scripts/Powershell/Gather-EncryptionStatus.ps1
@@ -0,0 +1,39 @@
+﻿## Kaseya Automation Team
+## Used by the "Gather Encryption Status" Agent Procedure
+param (
+    [parameter(Mandatory=$true)]
+    [string]$AgentName = "",
+    [parameter(Mandatory=$true)]
+	[string]$Path = ""
+)
+
+$Output = New-Object psobject
+
+Add-Member -InputObject $Output -MemberType NoteProperty -Name MachineID -Value $AgentName
+
+$BitLockerStatus = (Get-BitLockerVolume |Select-Object -Property ProtectionStatus).ProtectionStatus
+
+if ($BitLockerStatus -eq "On") {
+    Add-Member -InputObject $Output -MemberType NoteProperty -Name BitLockerStatus -Value "True"
+} else {
+    Add-Member -InputObject $Output -MemberType NoteProperty -Name BitLockerStatus -Value "False"
+}
+
+$RemovableDevices = Get-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\FVE\" -Name RDVConfigureBDE -ErrorAction SilentlyContinue
+
+if ($RemovableDevices) {
+    Add-Member -InputObject $Output -MemberType NoteProperty -Name BitLockerRemovableDeviceStatus -Value "True"
+} else {
+    Add-Member -InputObject $Output -MemberType NoteProperty -Name BitLockerRemovableDeviceStatus -Value "False"
+}
+
+$LegalNoticeCaption = (Get-ItemProperty -Path "HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system\” -Name legalnoticecaption).legalnoticecaption
+
+If ($LegalNoticeCaption) {
+    Add-Member -InputObject $Output -MemberType NoteProperty -Name LegalNoticeMessage -Value "True"
+}
+else {
+    Add-Member -InputObject $Output -MemberType NoteProperty -Name LegalNoticeMessage -Value "False"
+}
+
+Export-Csv -InputObject $Output -Path $Path -NoTypeInformation

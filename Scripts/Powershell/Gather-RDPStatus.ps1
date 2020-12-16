9df7ad05bda3b8745acf337ba17746132103a7cb
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2020-12-16 14:37:43 +0300

    Gather RDP status and check if Bombar installed
    PowerShell script gathers information if RDP enabled on the system and if Bombar Jump Client software is installed.  Exports gathered information into CSV file.

---
+++ Scripts/Powershell/Gather-RDPStatus.ps1
@@ -0,0 +1,36 @@
+﻿## Kaseya Automation Team
+## Used by the "Gather RDP enablement status" Agent Procedure
+
+param (
+    [parameter(Mandatory=$true)]
+    [string]$AgentName = "",
+    [parameter(Mandatory=$true)]
+	[string]$Path = ""
+)
+
+$Output = New-Object psobject
+
+Add-Member -InputObject $Output -MemberType NoteProperty -Name MachineID -Value $AgentName
+
+#Check if software is installed
+$isInstalled = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*) | Where-Object {$_.DisplayName -like "*Bombar*"}
+
+if ($isInstalled) {
+    #If it's installed, add installed and version properties to an object
+    Add-Member -InputObject $Output -TypeName Installed -MemberType NoteProperty -Name JumpClient -Value "True"
+}
+ else {
+    #If not installed, set values to False
+    Add-Member -InputObject $Output -TypeName Installed -MemberType NoteProperty -Name JumpClient -Value "False"
+}
+
+#Check if RDP connections are allowed
+$RDPStatus = (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name fDenyTSConnections | Select-Object -Property fDenyTSConnections).fDenyTSConnections
+
+If ($RDPStatus) {
+    Add-Member -InputObject $Output -MemberType NoteProperty -Name RDPEnabled -Value "False"
+} else {
+    Add-Member -InputObject $Output -MemberType NoteProperty -Name RDPEnabled -Value "True"
+}
+
+Export-Csv -InputObject $Output -Path $Path -NoTypeInformation

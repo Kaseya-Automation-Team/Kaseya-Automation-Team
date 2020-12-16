a36d93b932eb9b8abdcf59de11a328f5430da842
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2020-12-16 21:05:29 +0300

    Add files via upload

---
+++ Scripts/Powershell/Gather-PasswordPolicy.ps1
@@ -0,0 +1,118 @@
+﻿<#
+.Synopsis
+   Gathers password settings.
+   Uses standard secedit.exe utility.
+.DESCRIPTION
+   Gathers password settings and saves information to a CSV-file
+.EXAMPLE
+   .\Gather-PasswordPolicy.ps1 -AgentName '12345' -FileName 'password_settings.csv' -Path 'C:\TEMP'
+.NOTES
+   Version 0.1
+   Author: Vladislav Semko
+   Email: Vladislav.Semko@kaseya.com
+#>
+
+param (
+    [parameter(Mandatory=$true)]
+    [string]$AgentName,
+    [parameter(Mandatory=$true)]
+    [string]$FileName,
+    [parameter(Mandatory=$true)]
+    [string]$Path
+ )
+
+#region functions
+Function Get-SecurityPolicy { 
+[CmdletBinding()]  
+Param(  
+    [ValidateNotNullOrEmpty()]
+    [Parameter(ValueFromPipeline=$True,Mandatory=$True)]  
+    [string]$FileName  
+)
+#run secedit and save output
+    secedit /export /cfg "$SeceditOutput" | Out-Null
+              
+    [hashtable]$Content = @{}  
+    switch -regex -file $SeceditOutput 
+    {  
+        "^\[(.+)\]$" # Section  
+        {  
+            $section = $matches[1]  
+            $Content[$section] = @{}  
+            $CommentCount = 0  
+        }  
+        "^(;.*)$" # Comment  
+        {  
+            if (!($section))  
+            {  
+                $section = "No-Section"  
+                $Content[$section] = @{}  
+            }  
+            $value = $matches[1]  
+            $CommentCount = $CommentCount + 1  
+            $name = "Comment" + $CommentCount  
+            $Content[$section][$name] = $value  
+        }   
+        "(.+?)\s*=\s*(.*)" # Key  
+        {  
+            if (!($section))  
+            {  
+                $section = "No-Section"  
+                $Content[$section] = @{}  
+            }  
+            $name,$value = $matches[1..2]  
+            $Content[$section][$name] = $value  
+        }  
+    }
+    Return $Content
+}
+
+function Update-HashTable {
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory,ValueFromPipeline)][Hashtable]$Hashtable,
+    [String[]]$Include = ($HashTable.Keys),
+    [String[]]$Exclude
+)
+
+    if (-not $Include) {$Include = $HashTable.Keys}
+
+    $filteredHashTable = @{}
+    $HashTable.keys.where{
+        $PSItem -in $Include
+    }.where{
+        $PSItem -notin $Exclude
+    }.foreach{
+        $filteredHashTable[$PSItem] = $HashTable[$PSItem]
+    }
+    return $FilteredHashTable
+}
+#endregion functions
+
+[string]$currentDate = Get-Date -UFormat "%m/%d/%Y %T"
+[string]$SeceditOutput
+
+if ( $FileName -notmatch '\.csv$') { $FileName += '.csv' }
+if (-not [string]::IsNullOrEmpty( $Path) )
+{
+    $FileName = "$Path\$FileName"
+    $SeceditOutput = "$Path\secedit_out.cfg"
+}
+
+[string[]]$IncludeSettings = @('PasswordHistorySize', 'MinimumPasswordAge', 'MaximumPasswordAge', 'MinimumPasswordLength', 'PasswordComplexity', 'ClearTextPassword')
+
+[hashtable]$PasswordSettings = (Get-SecurityPolicy -FileName $FileName).'System Access' | Update-HashTable -Include $IncludeSettings
+
+[pscustomobject]$PasswordSettings | Select-Object -Property `
+@{Name = 'AgentGuid'; Expression = {$AgentName}}, `
+@{Name = 'Hostname'; Expression= {$env:COMPUTERNAME}}, `
+@{Name = 'Date'; Expression = {$currentDate}}, `
+@{Name = 'EnforcePasswordHistory'; Expression = {$_.PasswordHistorySize}}, `
+@{Name = 'MinimumPasswordAge'; Expression = {$_.MinimumPasswordAge}}, `
+@{Name = 'MaximumPasswordAge'; Expression = {$_.MaximumPasswordAge}}, `
+@{Name = 'MinimumPasswordLength'; Expression = {$_.MinimumPasswordLength}}, `
+@{Name = 'StorePasswordUsingReversibleEncryption'; Expression = {$_.ClearTextPassword}} `
+| Export-Csv -Path "FileSystem::$FileName" -Force -Encoding UTF8 -NoTypeInformation
+
+#cleanup
+if (test-Path $SeceditOutput) { Remove-Item $SeceditOutput -Force -Confirm:$false | Out-Null}

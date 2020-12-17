5682eddd90a76b4a925633a7821acbaf9e3a6c03
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2020-12-17 19:22:28 +0300

    Add files via upload
    Kaiser's task

---
+++ Scripts/Powershell/Gather-TimeZoneSettings.ps1
@@ -0,0 +1,53 @@
+﻿<#
+.Synopsis
+   Gathers time settings.
+.DESCRIPTION
+   Gathers time settings and saves information to a CSV-file
+.EXAMPLE
+   .\Gather-TimeZoneSettings.ps1 -AgentName '12345' -FileName 'timezone_settings.csv' -Path 'C:\TEMP'
+.NOTES
+   Version 0.1
+   Author: Vladislav Semko
+   Email: Vladislav.Semko@kaseya.com
+#>
+param (
+    [parameter(Mandatory=$true)]
+    [string]$AgentName,
+    [parameter(Mandatory=$true)]
+    [string]$FileName,
+    [parameter(Mandatory=$true)]
+    [string]$Path
+ )
+
+[string]$currentDate = Get-Date -UFormat "%m/%d/%Y %T"
+
+if ( $FileName -notmatch '\.csv$') { $FileName += '.csv' }
+if (-not [string]::IsNullOrEmpty( $Path) ) { $FileName = "$Path\$FileName" }
+
+#get NTP peers
+$PeersNTP = $(w32tm.exe /query /peers) 
+
+[string]$NTPServer = $(
+    if ( $PeersNTP -match 'error')
+    {
+        'NULL'
+    }
+    else
+    {
+        [string]$regexp = '(Peer:\s+)(\S+)?$' # look for string that starts with 'Peer:'
+        $PeersNTP | `
+        Foreach-Object {[regex]::match( $_, $regexp ).Groups[2].Value} | `  # get server name in the matching string
+        Where-Object { -not [string]::IsNullOrWhiteSpace($_)} | `   #get rid of empty lines
+        Select-Object -Unique | Select-Object -First 1  #get the first server
+    }
+)
+
+
+[pscustomobject]@{
+AgentGuid = $AgentName
+Hostname = $env:COMPUTERNAME
+Date = $currentDate
+Offset = $( [System.TimeZoneInfo]::Local.BaseUtcOffset ).ToString()
+DST = $( [System.TimeZoneInfo]::Local.SupportsDaylightSavingTime ).ToString()
+Server = $NTPServer
+} | Export-Csv -Path "FileSystem::$FileName" -Force -Encoding UTF8 -NoTypeInformation

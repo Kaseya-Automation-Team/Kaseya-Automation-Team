fd218e7a7463934bf4811f594207d02fbaddb9d2
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2020-12-22 19:16:27 +0300

    Add files via upload

---
+++ Scripts/Powershell/Run-AsSystem.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.Synopsis
+   Creates and runs as a system the script provided
+.DESCRIPTION
+   The scheduled task is created and launched in order to run provided script as the local system
+.EXAMPLE
+   .\Run-AsSystem -ScheduledScriptName 'Wipe-TheSystem.ps1'
+.NOTES
+   Version 0.1
+   Author: Proserv Team - VS
+#>
+
+param (
+    [parameter(Mandatory=$true)]
+    [string]$ScheduledScriptName
+ )
+
+[string]$TaskName = "Schedule $ScheduledScriptName"
+
+$ScheduledTaskPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
+$SettingsSet = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd
+$ScheduledTaskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date)
+
+$ScheduledTaskActionParams = @{
+    Execute = "PowerShell.exe" 
+    Argument = "-NoProfile -ExecutionPolicy Bypass -NonInteractive -Command $ScheduledScriptName"
+}
+$ScheduledTaskAction = New-ScheduledTaskAction @ScheduledTaskActionParams
+
+Register-ScheduledTask -Force -Principal $ScheduledTaskPrincipal -Trigger $ScheduledTaskTrigger -TaskName $TaskName -Action $ScheduledTaskAction -Settings $SettingsSet
+
+Start-ScheduledTask -TaskName $TaskName

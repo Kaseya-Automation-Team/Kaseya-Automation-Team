184c99c9b969aa3ff9782e8158ac4ba87016a25c
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2020-12-22 15:18:46 +0300

    Add IsLocalAdmin check
    Logon date format now include ms.
    Script can not only check is user is member of Administrator group, but also marks admin user, which is not member of Administrator group :)

--- Scripts/Powershell/Gather-LocalUserInfo.ps1
+++ Scripts/Powershell/Gather-LocalUserInfo.ps1
@@ -1,20 +1,19 @@
 ﻿## Kaseya Automation Team
 ## Used by the "Gather Local User Info" Agent Procedure
 
-<#param (
+param (
     [parameter(Mandatory=$true)]
     [string]$AgentName = "",
     [parameter(Mandatory=$true)]
 	[string]$Path = ""
 )
-#>
-$AgentName = "test"
-$Path = "c:\temp\test.csv"
 
 #Create array where all objects for export will be storred
 $Results = @()
 
 $LocalUsers = Get-WmiObject -Class Win32_UserAccount -Filter "LocalAccount='True'"
+
+$AdminGroupName = get-wmiobject win32_group | where {$_.sid -eq 'S-1-5-32-544'} | select Name -ExpandProperty Name
 
 ForEach ($User in $LocalUsers){
     
@@ -29,17 +28,34 @@ ForEach ($User in $LocalUsers){
     if ($LastLogonString -ne "Never") {
 
         $LastLogonString = $LastLogonString|Get-Date
-        $LastLogonString = Get-Date $LastLogonString -Format 'MM-dd-yyyy HH:mm:ss'
+        $LastLogonString = Get-Date $LastLogonString -Format 'MM-dd-yyyy HH:mm:ss:ms'
         $LastLogonString = $LastLogonString -replace "-", "/"
+
     }
 
+    $env:COMPUTERNAME | % {
+    $Group = [ADSI]("WinNT://$_/$AdminGroupName,group")
+    $Group.PSBase.Invoke('Members') | % {
+        $UserIsAdmin = $_.GetType().InvokeMember('Name', 'GetProperty', $null, $_, $null) | Select-String -Pattern $User.Name
+        }
+    }
+
+    if ($UserIsAdmin) {
+        Add-Member -InputObject $Output -MemberType NoteProperty -Name IsLocalAdmin -Value "True"
+    } else {
+
+        if ($User.SID -like "S-1-5-21-*-500") {
+            Add-Member -InputObject $Output -MemberType NoteProperty -Name IsLocalAdmin -Value "True"
+        } else {
+            Add-Member -InputObject $Output -MemberType NoteProperty -Name IsLocalAdmin -Value "False"
+        }
+    }
 
     Add-Member -InputObject $Output -MemberType NoteProperty -Name LastLogon -Value $LastLogonString
 
     #Add object to the previously created array
     $Results += $Output
 }
-
 
 #Export results to csv file
 $Results| Export-Csv -Path $Path -NoTypeInformation -Encoding UTF8

8ca502fce9dd7f777b5590706131d2732019cebe
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2020-12-22 12:27:41 +0300

    Fix date/time format
    Timestamp now is in 24 hour format, instead of 12 hour

--- Scripts/Powershell/Gather-LocalUserInfo.ps1
+++ Scripts/Powershell/Gather-LocalUserInfo.ps1
@@ -1,12 +1,15 @@
 ﻿## Kaseya Automation Team
 ## Used by the "Gather Local User Info" Agent Procedure
 
-param (
+<#param (
     [parameter(Mandatory=$true)]
     [string]$AgentName = "",
     [parameter(Mandatory=$true)]
 	[string]$Path = ""
 )
+#>
+$AgentName = "test"
+$Path = "c:\temp\test.csv"
 
 #Create array where all objects for export will be storred
 $Results = @()
@@ -22,6 +25,15 @@ ForEach ($User in $LocalUsers){
     Add-Member -InputObject $Output -MemberType NoteProperty -Name Disabled -Value $User.Disabled
     
     $LastLogonString = (net user $User.Name | findstr /B /C:"Last logon").trim("Last logon                   ")
+
+    if ($LastLogonString -ne "Never") {
+
+        $LastLogonString = $LastLogonString|Get-Date
+        $LastLogonString = Get-Date $LastLogonString -Format 'MM-dd-yyyy HH:mm:ss'
+        $LastLogonString = $LastLogonString -replace "-", "/"
+    }
+
+
     Add-Member -InputObject $Output -MemberType NoteProperty -Name LastLogon -Value $LastLogonString
 
     #Add object to the previously created array

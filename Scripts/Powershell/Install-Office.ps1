f3c0da26a39dcc5f7d0b7014f532e8c9c9798eb8
vlad <vladislav.semko@kaseya.com>
2021-01-08 15:35:20 +0300

    Update Install-Office.ps1
    Config generated inside the script

--- Scripts/Powershell/Install-Office.ps1
+++ Scripts/Powershell/Install-Office.ps1
@@ -4,7 +4,7 @@
 .DESCRIPTION
    Prepare config file and call office installation using ODT setup
 .EXAMPLE
-   .\Install-Office.ps1 -$ConfigFile 'Config32.xml' -DownloadTo '\\Server\Share'
+   .\Install-Office.ps1 -DownloadTo '\\Server\Share' -BitVersion '64' -OfficeEdition 'ProPlusRetail'
 .NOTES
    Version 0.1
    Author: Proserv Team - VS
@@ -13,12 +13,39 @@
 
 param (
     [parameter(Mandatory=$true)]
-    [string]$ConfigFile,
+    [string]$DownloadTo,
     [parameter(Mandatory=$true)]
-    [string]$DownloadTo
- )
-$DownloadTo = "\\W2012R2-T1\Software"
-[xml]$xmlConfig = Get-Content -Path $ConfigFile
-$xmlConfig.Configuration.Add.SourcePath = $DownloadTo
-$xmlConfig.Save($ConfigFile)
-#& .\install.cmd
+    [string]$BitVersion,
+    [parameter(Mandatory=$true)]
+    [string]$OfficeEdition
+)
+
+#region creating the batch file
+@"
+@echo off
+cd /D "%~dp0"
+
+set LOGFILE=install_cmd.log
+call :LOG > %LOGFILE%
+exit /B
+
+:LOG
+setup.exe /download Config.xml
+setup.exe /configure Config.xml
+"@ | Out-File -FilePath "install.cmd" -Force -Encoding utf8
+#endregion creating the batch file
+
+#region creating the config file
+@"
+<Configuration>
+  <Add SourcePath="{0}" OfficeClientEdition="{1}">
+    <Product ID="{2}">
+      <Language ID="en-us" />
+    </Product>
+  </Add>
+  <!--  <Updates Enabled="FALSE" Branch="Current" /> -->
+  <Display Level="None" AcceptEULA="TRUE" />
+  <Property Name="AUTOACTIVATE" Value="0" />
+</Configuration>
+"@ -f @($DownloadTo, $BitVersion, $OfficeEdition) | Out-File -FilePath "Config.xml" -Force -Encoding utf8
+#endregion creating the batch file

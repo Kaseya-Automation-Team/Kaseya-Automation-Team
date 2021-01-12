e07a207b38850be9f7c52ae8678ca2a25518ab8e
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-01-12 17:24:31 +0300

    Optimize performance and add DEBUG
    Script doesn't use WMI queries anymore and gets list of admin group members only once, at first run, but not on each run of cycle.

--- Scripts/Powershell/Gather-LocalUserInfo.ps1
+++ Scripts/Powershell/Gather-LocalUserInfo.ps1
@@ -5,57 +5,103 @@ param (
     [parameter(Mandatory=$true)]
     [string]$AgentName = "",
     [parameter(Mandatory=$true)]
-	[string]$Path = ""
+	[string]$Path = "",
+    [parameter(Mandatory=$true)]
+	[string]$Filename = "",
+    [parameter(Mandatory=$false)]
+    [int]$Limit = 3,
+    [parameter(Mandatory=$false)]
+    [int]$LogIt = 1
 )
+
+[string]$Pref = "Continue"
+if (1 -eq $LogIt)
+{
+    $DebugPreference = $Pref
+    $VerbosePreference = $Pref
+    $InformationPreference = $Pref
+    $ScriptName = [io.path]::GetFileNameWithoutExtension( $($MyInvocation.MyCommand.Name) )
+    $LogFile = "$Path\$ScriptName.log"
+    Start-Transcript -Path $LogFile
+}
+
+Write-Debug "Script execution started"
 
 #Create array where all objects for export will be storred
 $Results = @()
 
-$LocalUsers = Get-WmiObject -Class Win32_UserAccount -Filter "LocalAccount='True'"
+$LocalUsers = Get-LocalUser
 
-$AdminGroupName = get-wmiobject win32_group | where {$_.sid -eq 'S-1-5-32-544'} | select Name -ExpandProperty Name
+Write-Debug ($LocalUsers| Select-Object * | Out-String)
+
+$AdminGroup = Get-LocalGroupMember -SID 'S-1-5-32-544' | Where-Object {$_.PrincipalSource -eq "Local"}
+
+Write-Debug ($AdminGroup | Select-Object * |Out-String)
+
+$Counter = 0
 
 ForEach ($User in $LocalUsers){
+
+    $Counter = $Counter+1
+
+    Write-Debug $Counter
+    Write-Debug $User.Name
     
     $Output = New-Object psobject
 
     Add-Member -InputObject $Output -MemberType NoteProperty -Name MachineID -Value $AgentName
     Add-Member -InputObject $Output -MemberType NoteProperty -Name UserName -Value $User.Name
-    Add-Member -InputObject $Output -MemberType NoteProperty -Name Disabled -Value $User.Disabled
+    Add-Member -InputObject $Output -MemberType NoteProperty -Name Enabled -Value $User.Enabled
     
-    $LastLogonString = (net user $User.Name | findstr /B /C:"Last logon").trim("Last logon                   ")
+    $LastLogonString = $User.LastLogon
 
-    if ($LastLogonString -ne "Never") {
+     if ($LastLogonString) {
 
         $LastLogonString = $LastLogonString|Get-Date
         $LastLogonString = Get-Date $LastLogonString -Format 'MM-dd-yyyy HH:mm:ss:ms'
         $LastLogonString = $LastLogonString -replace "-", "/"
 
-    }
-
-    $env:COMPUTERNAME | % {
-    $Group = [ADSI]("WinNT://$_/$AdminGroupName,group")
-    $Group.PSBase.Invoke('Members') | % {
-        $UserIsAdmin = $_.GetType().InvokeMember('Name', 'GetProperty', $null, $_, $null) | Select-String -Pattern $User.Name
-        }
-    }
-
-    if ($UserIsAdmin) {
-        Add-Member -InputObject $Output -MemberType NoteProperty -Name IsLocalAdmin -Value "True"
     } else {
 
-        if ($User.SID -like "S-1-5-21-*-500") {
-            Add-Member -InputObject $Output -MemberType NoteProperty -Name IsLocalAdmin -Value "True"
-        } else {
-            Add-Member -InputObject $Output -MemberType NoteProperty -Name IsLocalAdmin -Value "False"
-        }
+        $LastLogonString = "Never"
+    }
+
+    Write-Debug ($LastLogonString|Out-String)
+
+    if ($AdminGroup.Name -contains "$env:COMPUTERNAME\$User")
+    {
+        Add-Member -InputObject $Output -MemberType NoteProperty -Name IsLocalAdmin -Value "True"
+
+    } else {
+
+        Add-Member -InputObject $Output -MemberType NoteProperty -Name IsLocalAdmin -Value "False"
     }
 
     Add-Member -InputObject $Output -MemberType NoteProperty -Name LastLogon -Value $LastLogonString
 
     #Add object to the previously created array
     $Results += $Output
+
+    If ($Limit -ne 0) {
+
+        if ($Counter -eq $Limit) {
+
+            Write-Debug "Limit of $Limit records has been reached"
+            Exit
+
+        }
+            
+    }
 }
 
 #Export results to csv file
-$Results| Export-Csv -Path $Path -NoTypeInformation -Encoding UTF8
+$Results| Export-Csv -Path $Path\$Filename -NoTypeInformation -Encoding UTF8
+
+if (1 -eq $LogIt)
+{
+    $Pref = "SilentlyContinue"
+    $DebugPreference = $Pref
+    $VerbosePreference = $Pref
+    $InformationPreference = $Pref
+    Stop-Transcript
+}

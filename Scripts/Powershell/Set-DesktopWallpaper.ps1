4e90f8b0c20c5c7b03317f60414e4f167432a0b8
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-02-05 16:38:22 +0300

    AP and PS script to update desktop wallpaper

---
+++ Scripts/Powershell/Set-DesktopWallpaper.ps1
@@ -0,0 +1,52 @@
+﻿## Kaseya Automation Team
+## Used by the "Set desktop wallpaper" Agent Procedure
+
+param (
+    [parameter(Mandatory=$true)]
+    [string]$FileName = "",
+    [parameter(Mandatory=$false)]
+    [int]$LogIt = 0
+
+ )
+
+[string]$Pref = "Continue"
+if (1 -eq $LogIt)
+{
+    $DebugPreference = $Pref
+    $VerbosePreference = $Pref
+    $InformationPreference = $Pref
+    $ScriptName = [io.path]::GetFileNameWithoutExtension( $($MyInvocation.MyCommand.Name) )
+    $LogFile = "$Path\$ScriptName.log"
+    Start-Transcript -Path $LogFile
+}
+
+Write-Debug "Script execution started"
+Write-Debug $FileName
+
+$Code = @' 
+using System.Runtime.InteropServices; 
+namespace Win32{ 
+    
+     public class Wallpaper{ 
+        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+         
+         public static void SetWallpaper(string thePath){ 
+            SystemParametersInfo(20,0,thePath,3); 
+         }
+    }
+ } 
+'@
+
+Add-type $Code 
+
+[Win32.Wallpaper]::SetWallpaper($FileName)
+
+if (1 -eq $LogIt)
+{
+    $Pref = "SilentlyContinue"
+    $DebugPreference = $Pref
+    $VerbosePreference = $Pref
+    $InformationPreference = $Pref
+    Stop-Transcript
+}

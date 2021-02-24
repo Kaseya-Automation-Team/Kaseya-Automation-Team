53a00e17a6f8e3c283bbecfc05d200e4cc8f3391
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-02-24 18:29:27 +0300

    DEBUG + documentation

--- VSA/Agent Procedures/Internet speed test/Speedtest/Test-InternetSpeed.ps1
+++ VSA/Agent Procedures/Internet speed test/Speedtest/Test-InternetSpeed.ps1
@@ -8,9 +8,27 @@ param (
     [int]$LogIt = 0
 )
 
+[string]$Pref = "Continue"
+if (1 -eq $LogIt)
+{
+    $DebugPreference = $Pref
+    $VerbosePreference = $Pref
+    $InformationPreference = $Pref
+    $LogFile = "$Path\Test-InternetSpeed.log"
+    Start-Transcript -Path $LogFile
+}
+
+Write-Debug "Script execution started"
+
 $Output = & $Path\ndt-test.exe -quiet -timeout 30s -format=json
 
-$Output = $Output | ConvertFrom-Json
+Write-Debug ($Output|Out-String)
+
+try {
+	$Output = $Output | ConvertFrom-Json
+} catch {
+	Write-Host "Unable to run speed test executables"
+}
 
 $Download = [math]::Round($Output.Download.Value, 2),$Output.Download.Unit | Out-String
 $Download.Replace("`r`n", " ") | Out-File -FilePath $Path\download_speedtest.txt
@@ -20,3 +38,12 @@ $Upload.Replace("`r`n", " ") | Out-File -FilePath $Path\upload_speedtest.txt
 
 $Latency = $Output.MinRTT.Value,$Output.MinRTT.unit | Out-String
 $Latency.Replace("`r`n", " ") | Out-File -FilePath $Path\latency_speedtest.txt
+
+if (1 -eq $LogIt)
+{
+    $Pref = "SilentlyContinue"
+    $DebugPreference = $Pref
+    $VerbosePreference = $Pref
+    $InformationPreference = $Pref
+    Stop-Transcript
+}

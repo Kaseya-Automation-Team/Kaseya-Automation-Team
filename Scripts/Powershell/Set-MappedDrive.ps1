5d4a6adb5d92e5f457b397525bc39c9127a49ef8
vlad <vladislav.semko@kaseya.com>
2021-03-01 17:34:23 +0300

    Update Set-MappedDrive.ps1
    regex, conditions refined.

--- Scripts/Powershell/Set-MappedDrive.ps1
+++ Scripts/Powershell/Set-MappedDrive.ps1
@@ -1,13 +1,14 @@
 # Requires -Version 5.1 
 <#
 .Synopsis
-  Creates/updates Mapped drive .
+  Creates/updates Mapped drive
 .DESCRIPTION
+   Creates or updates network drive for all the computer's users
    Used by Agent Procedure
 .EXAMPLE
-   .\Set-MappedDrive.ps1 -UNCPath '\\Server\Path' -DriveLetter 'X'
+   .\Set-MappedDrive.ps1 -UNCPath '\\Server\Share' -DriveLetter 'X' -UpdateMapping 1
 .EXAMPLE
-   .\Set-MappedDrive.ps1 -UNCPath '\\Server\Path' -DriveLetter 'X' -LogIt 0
+   .\Set-MappedDrive.ps1 -UNCPath '\\Server\Share' -DriveLetter 'X' -LogIt 0
 .NOTES
    Version 0.1
    Author: Proserv Team - VS
@@ -18,17 +19,19 @@ param (
                    ValueFromPipelineByPropertyName=$true, 
                    ValueFromRemainingArguments=$false, 
                    Position=0)]
-    [ValidatePattern('^\\\\[\w\-.]+\\[\w\-.\$]*$')]
+    [ValidatePattern( '^\\\\[\w\-.]+\\[\w\-.\$]*$' )]
     [string] $UNCPath,
     [parameter(Mandatory=$true, 
     ValueFromPipeline=$true,
     ValueFromPipelineByPropertyName=$true, 
     ValueFromRemainingArguments=$false, 
     Position=1)]
-    [ValidatePattern(' [a-zA-Z]')]
+    [ValidatePattern( '[h-zH-Z]' )]
     [string] $DriveLetter,
     [parameter(Mandatory=$false)]
-    [int] $LogIt = 2
+    [int] $LogIt = 1,
+    [parameter(Mandatory=$false)]
+    [int] $UpdateMapping = 0
 )
 
 #region function Set-RegParam
@@ -53,8 +56,14 @@ function Set-RegParam {
             ValueFromPipelineByPropertyName=$true, 
             ValueFromRemainingArguments=$false, 
             Position=2)]
-            [ValidateSet('Binary', 'DWord', 'ExpandString', 'MultiString', 'None', 'QWord', 'String', 'Unknown')]
-        [string] $ValueType = 'DWord'
+        [ValidateSet('Binary', 'DWord', 'ExpandString', 'MultiString', 'None', 'QWord', 'String', 'Unknown')]
+        [string] $ValueType = 'DWord',
+        [parameter(Mandatory=$false, 
+                ValueFromPipeline=$true,
+                ValueFromPipelineByPropertyName=$true, 
+                ValueFromRemainingArguments=$false, 
+                Position=3)]
+        [int] $UpdateExisting = 0
     )
     
     begin {
@@ -68,20 +77,23 @@ function Set-RegParam {
             if( -not (Test-Path -Path $RegKey) )
             {
                 try {
-                    New-Item -Path $RegKey -Force  -ErrorAction Stop
+                    New-Item -Path $RegKey -Force -Verbose -ErrorAction Stop
                 } catch { Write-Error $_.Exception.Message}
             }
             #Create property
             try {
-                New-ItemProperty -Path $RegKey -Name $RegProperty -PropertyType $ValueType -Value $RegValue -Force  -ErrorAction Stop
+                New-ItemProperty -Path $RegKey -Name $RegProperty -PropertyType $ValueType -Value $RegValue -Force -Verbose -ErrorAction Stop
             } catch { Write-Error $_.Exception.Message}
         }
         else
         {
             #Assign value to the property
-            try {
-                Set-ItemProperty -Path $RegKey -Name $RegProperty -Value $RegValue -Force -ErrorAction Stop
-            } catch {Write-Error $_.Exception.Message}
+            if( 1 -eq $UpdateExisting)
+            {
+                try {
+                        Set-ItemProperty -Path $RegKey -Name $RegProperty -Value $RegValue -Force -Verbose -ErrorAction Stop
+                    } catch {Write-Error $_.Exception.Message}
+            }
         }
     }
 }
@@ -105,7 +117,7 @@ if ( 1 -eq $LogIt )
 [array] $RegParameters = @(
     [PSCustomObject] @{ChildPath = "Network\$DriveLetter\RemotePath"; Value = $UNCPath; Type = 'String'},
     [PSCustomObject] @{ChildPath = "Network\$DriveLetter\UserName"; Value = ""; Type = 'String'},
-    [PSCustomObject] @{ChildPath = "Network\$DriveLetter\ProviderName"; Value = "Microsoft Windows Network"; Type = 'String'},
+    [PSCustomObject] @{ChildPath = "Network\$DriveLetter\ProviderName"; Value = 'Microsoft Windows Network'; Type = 'String'},
     [PSCustomObject] @{ChildPath = "Network\$DriveLetter\ProviderType"; Value = '0x002000'; Type = 'DWord'},
     [PSCustomObject] @{ChildPath = "Network\$DriveLetter\ConnectionType"; Value = '0x000001'; Type = 'DWord'},
     [PSCustomObject] @{ChildPath = "Network\$DriveLetter\ConnectFlags"; Value = '0x000000'; Type = 'DWord'},
@@ -120,10 +132,10 @@ if ( 1 -eq $LogIt )
 [string] $RegKeyUserProfiles = 'HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*'
 
 [array] $ProfileList = Get-ItemProperty -Path Registry::$RegKeyUserProfiles | `
-    Select-Object  @{name="SID";expression={$_.PSChildName}}, 
-            @{name="UserHive";expression={"$($_.ProfileImagePath)\ntuser.dat"}}, 
-            @{name="UserName";expression={$_.ProfileImagePath -replace '^(.*[\\\/])', ''}} | `
-            Where-Object {$_.SID -match $SIDPattern}
+                    Select-Object  @{name="SID";expression={$_.PSChildName}}, 
+                    @{name="UserHive";expression={"$($_.ProfileImagePath)\ntuser.dat"}}, 
+                    @{name="UserName";expression={$_.ProfileImagePath -replace '^(.*[\\\/])', ''}} | `
+                    Where-Object {$_.SID -match $SIDPattern}
 
 # Get all user SIDs found in HKEY_USERS (ntuder.dat files that are loaded)
 $LoadedHives = Get-ChildItem Registry::HKEY_USERS | `
@@ -137,9 +149,9 @@ $HivesToLoad = Compare-Object -ReferenceObject $ProfileList.SID -DifferenceObjec
 # Loop through each profile on the machine
 Foreach ($Profile in $ProfileList) {
     # Load User ntuser.dat if it's not already loaded
-    if ($Profile.SID -in $HivesToLoad)
+    if ( $Profile.SID -in $HivesToLoad )
     {
-        reg load "HKU\$($Profile.SID)" "$($Profile.UserHive)" | Out-Null
+        reg load "HKU\$($Profile.SID)" "$($Profile.UserHive)"
     }
  
     #####################################################################
@@ -147,8 +159,8 @@ Foreach ($Profile in $ProfileList) {
     "{0} {1}" -f "`tUser:", $($Profile.UserName) | Write-Verbose
     foreach($item in $RegParameters)
     {
-        [string] $RegPath = Join-Path -Path "HKEY_USERS\$($Profile.SID)" -ChildPath $($item.ChildPath)
-        Set-RegParam -RegPath $RegPath -RegValue "$($item.Value)" -ValueType $($item.Type)
+        [string] $RegPath = Join-Path -Path "HKEY_USERS\$($Profile.SID)" -ChildPath $($item.ChildPath) -Verbose
+        Set-RegParam -RegPath $RegPath -RegValue $($item.Value) -ValueType $($item.Type)
     }
     #####################################################################
  

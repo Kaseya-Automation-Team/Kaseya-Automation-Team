addded630fa562e64b05f53b14d8cecfb09d5bad
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-04-01 16:39:29 +0300

    PS scripts for Watchdog

---
+++ VSA/Agent Procedures/Kaseya Agent Watchdog/Task-Scheduler.ps1
@@ -0,0 +1,63 @@
+﻿## Kaseya Automation Team
+## Used by the "Kaseya Agent Watchdog" Agent Procedure
+
+param (
+    [parameter(Mandatory=$true)]
+	[string]$Path = "",
+    [parameter(Mandatory=$false)]
+    [int]$LogIt = 0
+)
+
+[string]$Pref = "Continue"
+if (1 -eq $LogIt)
+{
+    $DebugPreference = $Pref
+    $VerbosePreference = $Pref
+    $InformationPreference = $Pref
+    $ScriptName = [io.path]::GetFileNameWithoutExtension( $($MyInvocation.MyCommand.Name) )
+    $LogFile = "$Path\$ScriptName.log"
+    Start-Transcript -Path $LogFile
+}
+
+#Path to Kaseya Agent Watchdog script
+$ScriptPath = "$Path\Watch-AgentService.ps1"
+
+#Check if task already exists
+$TaskExists = Get-ScheduledTask -TaskName "Kaseya Agent Watchdog" -ErrorAction SilentlyContinue
+
+if ($TaskExists) {
+    Write-Host "Task already exists - no need to create."
+    Write-Debug($TaskExists|Out-String)
+} else {
+    Write-Host "Task doesn't exist and will be created"
+
+    try {
+
+        #If task doesn't exist, create it using specified options
+        $TaskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-file ""$ScriptPath`""
+
+        $Repeat = (New-TimeSpan -Minutes 5)
+
+        $TaskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval $Repeat
+
+        Register-ScheduledTask -TaskName "Kaseya Agent Watchdog" -User "System" -Action $TaskAction -Trigger $TaskTrigger -Description "Restart Kaseya Agent service if it's not in Running state" -ErrorAction Stop
+
+        Write-Host "Task has been successfully created."
+
+        Start-ScheduledTask -TaskName "Kaseya Agent Watchdog"
+
+    } catch {
+        #Handle error
+        Write-Host "Unable to create task."
+        Write-Host $_.Exception.Message
+    }
+}
+
+if (1 -eq $LogIt)
+{
+    $Pref = "SilentlyContinue"
+    $DebugPreference = $Pref
+    $VerbosePreference = $Pref
+    $InformationPreference = $Pref
+    Stop-Transcript
+}
---
+++ VSA/Agent Procedures/Kaseya Agent Watchdog/Watch-AgentService.ps1
@@ -0,0 +1,41 @@
+﻿## Kaseya Automation Team
+## Used by the "Kaseya Agent Watchdog" Agent Procedure
+
+#Get folder where script remains
+$ScriptDir = Split-Path ($MyInvocation.MyCommand.Path) -Parent
+
+#Get name of the script
+$ScriptName = [io.path]::GetFileNameWithoutExtension( $($MyInvocation.MyCommand.Name) )
+
+#Combine path to the log file
+$Log = "$ScriptDir\$ScriptName.log"
+
+#Implement logging with date/time
+Function Log {
+    param(
+        [Parameter(Mandatory=$true)][String]$msg
+    )
+    $DateTime = Get-Date -Format "dd.mm.yyyy hh:mm:ss.ms"
+    Add-Content $Log ($DateTime + " " + $msg)
+
+    #Uncomment line below, if you also want log entries to be displayed under Procedure History log, not only recorded to log file
+    #Write-Host $msg
+}
+
+#Check if Kaseya Agent service is present and identify it's Name (instead of Display Name)
+$ServiceName = Get-Service -DisplayName 'Kaseya Agent' | Select-Object -ExpandProperty Name
+
+#Get status of Kaseya Agent service
+$isRunning = Get-Service -Name $ServiceName
+
+if ($isRunning.Status -eq "Stopped") {
+    #If status is stopped - restart
+    Log("Service is not running, trying to restart")
+    Restart-Service -Name $ServiceName
+} else {
+    if ($isRunning.Status -eq "Running") {
+       #If status is running - do nothing
+       Log("Service is already running - not restarting")
+    }
+    
+}

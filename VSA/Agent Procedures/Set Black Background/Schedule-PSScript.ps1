d6e44ba8340484a4449689092c66eb7d5d60ce61
vlad <vladislav.semko@kaseya.com>
2021-05-21 18:09:52 +0300

    Set Black Background

---
+++ VSA/Agent Procedures/Set Black Background/Schedule-PSScript.ps1
@@ -0,0 +1,16 @@
+﻿param (
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ScheduledTaskAction,
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $TaskName
+)
+
+if ( $null -eq $(try {Get-ScheduledTask -TaskName $TaskName -ErrorAction Stop} Catch {$null}) )
+{
+    $Trigger = New-ScheduledTaskTrigger -AtLogon
+    $Principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Users"
+    $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $ScheduledTaskAction
+    Register-ScheduledTask -TaskName 'SetBackgroundBlack' -Action $Action -Principal $Principal -Trigger $Trigger
+}

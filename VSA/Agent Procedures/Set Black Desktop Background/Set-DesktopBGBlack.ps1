24ee1c27ffc565197953957326b6f98c342c975d
vlad <vladislav.semko@kaseya.com>
2021-06-07 15:54:13 +0300

    Update Set-DesktopBGBlack.ps1
    Added disabling GPOs

--- VSA/Agent Procedures/Set Black Desktop Background/Set-DesktopBGBlack.ps1
+++ VSA/Agent Procedures/Set Black Desktop Background/Set-DesktopBGBlack.ps1
@@ -1,4 +1,70 @@
-﻿#region class Wallpaper
+﻿#region function Set-RegParam
+function Set-RegParam {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$true, 
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true, 
+            ValueFromRemainingArguments=$false, 
+            Position=0)]
+        [string] $RegPath,
+        [parameter(Mandatory=$true, 
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true, 
+            ValueFromRemainingArguments=$false, 
+            Position=1)]
+        [AllowEmptyString()]
+        [string] $RegValue,
+        [parameter(Mandatory=$false, 
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true, 
+            ValueFromRemainingArguments=$false, 
+            Position=2)]
+        [ValidateSet('Binary', 'DWord', 'ExpandString', 'MultiString', 'None', 'QWord', 'String', 'Unknown')]
+        [string] $ValueType = 'DWord',
+        [parameter(Mandatory=$false)]
+        [Switch] $UpdateExisting
+    )
+    
+    begin {
+        [string] $RegKey = Split-Path -Path Registry::$RegPath -Parent
+        [string] $RegProperty = Split-Path -Path Registry::$RegPath -Leaf
+    }
+    process {
+            #Create key
+            if( -not (Test-Path -Path $RegKey) )
+            {
+                try {
+                    New-Item -Path $RegKey -Force -Verbose -ErrorAction Stop
+                } catch { Write-Error $_.Exception.Message}
+                #Create property
+                try {
+                    New-ItemProperty -Path $RegKey -Name $RegProperty -PropertyType $ValueType -Value $RegValue -Force -Verbose -ErrorAction Stop
+                } catch { Write-Error $_.Exception.Message}
+            }            
+            else
+            {
+                $Poperty = try {Get-ItemProperty -Path $RegPath -ErrorAction Stop | Out-Null} catch { $null}
+                if ($null -eq $Poperty )
+                {
+                     #Create property
+                    try {
+                        New-ItemProperty -Path $RegKey -Name $RegProperty -PropertyType $ValueType -Value $RegValue -Force -Verbose -ErrorAction Stop
+                    } catch { Write-Error $_.Exception.Message}
+                }
+                #Assign value to the property
+                if( $UpdateExisting )
+                {
+                    try {
+                            Set-ItemProperty -Path $RegKey -Name $RegProperty -Value $RegValue -Force -Verbose -ErrorAction Stop
+                        } catch {Write-Error $_.Exception.Message}
+                }
+            }
+    }
+}
+#endregion function Set-RegParam
+
+#region class Wallpaper
 Add-Type @' 
 using System.Runtime.InteropServices; 
 namespace Win32{ 
@@ -21,11 +87,27 @@ namespace Win32{
 Get-WmiObject Win32_UserProfile | Where-Object {$_.SID -match $SIDPattern} | Select-Object LocalPath, SID | `
     ForEach-Object {
         $UserProfilePath = $_.LocalPath
-
+  
         reg load "HKU\$($_.SID)" "$UserProfilePath\ntuser.dat"
 
-        Set-ItemProperty -Path Registry::$(Join-Path -Path "HKEY_USERS\$($_.SID)" -ChildPath "Control Panel\Desktop") -Name "Wallpaper" -Value ''
-        Set-ItemProperty -Path Registry::$(Join-Path -Path "HKEY_USERS\$($_.SID)" -ChildPath "Control Panel\Colors") -Name "Background" -Value '0 0 0'
+        #Find & remove applied GPO Wallpaper settings
+        [string] $PropertyName = 'WallPaper'
+        Get-ChildItem -Path Registry::$(Join-Path -Path "HKEY_USERS\$($_.SID)" -ChildPath "SOFTWARE\Microsoft\Windows\CurrentVersion") -Recurse -ErrorAction SilentlyContinue | `
+                Where-Object {$_.PSPath -match 'Policies'} | `
+                ForEach-Object {
+                    $Key = $_ 
+                    if( $($Key.GetValueNames()) -match $PropertyName)
+                    { 
+                        try {
+                            Remove-ItemProperty -Path Registry::$Key -Name $PropertyName -Force -Verbose -ErrorAction Stop
+                            }
+                        catch {Write-Error $_.Exception.Message}
+                    }
+                }
+
+        #Apply New Settings
+        Set-RegParam -RegPath $(Join-Path -Path "HKEY_USERS\$($_.SID)" -ChildPath "Control Panel\Desktop\Wallpaper") -RegValue '' -ValueType String -UpdateExisting
+        Set-RegParam -RegPath $(Join-Path -Path "HKEY_USERS\$($_.SID)" -ChildPath "Control Panel\Colors\Background") -RegValue '0 0 0' -ValueType String -UpdateExisting
         
         [Win32.Wallpaper]::SetWallpaper("")
         [gc]::Collect()

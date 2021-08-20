7f36c6f6db0b76bf3b84c0e4920f68057e7a965b
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-08-20 11:16:11 +0300

    Create Get-AutomationExchangeItems.ps1

---
+++ VSA/Agent Procedures/Get-AutomationExchangeItems.ps1
@@ -0,0 +1,17 @@
+﻿$CurrentPage = 1
+While ($CurrentPage -le 23){
+
+    Write-Host "Working on the page number $CurrentPage"
+
+    if ($CurrentPage -eq 1) {
+        $URL = "https://www.community.connectit.com/automation-exchange/categories/product"
+    } else {
+        $URL = "https://www.community.connectit.com/automation-exchange/categories/product/p$CurrentPage"
+    }
+
+    $Request = Invoke-WebRequest -Uri $URL
+    Write-Host $URL
+    $Request.Links|Where-Object {$_.class -eq "title"}|Select-Object -Property innerHTML,href|Export-Excel -Path c:\temp\test.xlsx -TableName AutomationExchange -Append
+
+    $CurrentPage++
+}

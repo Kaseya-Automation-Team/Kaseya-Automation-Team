d5b2c2c535757ad189f62557f083a53c47df82fd
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-08-27 18:52:35 +0300

    Change in structure
    And new methods - Get-VSARoles, Get-VSAScopes

---
+++ VSA/Agent Procedures/VSA Module/private/Log-Event.ps1
@@ -0,0 +1,19 @@
+function Log-Event {
+    param(        
+        [Parameter(Mandatory=$true)][String]$Msg,
+        [Parameter(Mandatory=$true)][Int]$Id,
+        [Parameter(Mandatory=$true)][String]$Type
+    )
+
+    #Check if log source alread exists
+    $SourceExists = [System.Diagnostics.EventLog]::SourceExists("VSA API Module")
+
+    #If not, create a new one
+    if ($SourceExists -eq $false) {
+        New-EventLog –LogName Application –Source “VSA API Module”
+    }
+
+    Write-EventLog –LogName Application –Source “VSA API Module” –EntryType $Type –EventID $Id  –Message $Msg -Category 0
+    $CurrentTime = Get-Date
+    Write-Host "$CurrentTime`: $Type`: $Msg"
+}

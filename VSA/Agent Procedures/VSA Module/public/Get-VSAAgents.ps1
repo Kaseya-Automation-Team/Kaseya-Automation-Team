939351802616b8f9dd30d5240ed4f2bd879cba32
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-08-31 23:19:27 +0300

    More functionality

---
+++ VSA/Agent Procedures/VSA Module/public/Get-VSAAgents.ps1
@@ -0,0 +1,53 @@
+function Get-VSAAgents
+{
+
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [VSAConnection] $VSAConnection,
+        [parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $SystemUsersSuffix = 'assetmgmt/agents',
+        [parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Filter,
+        [parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Paging,
+        [parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Sort
+    )
+
+    if ( $($VSAConnection.GetStatus()) -eq "Open") #if token is valid
+    {
+        $CombinedURL = "$($VSAConnection.URI)/$SystemUsersSuffix"
+        
+        if ($Filter) {
+            $CombinedURL = -join ($CombinedURL, "`?`$filter=$Filter")
+        }
+
+        if ($Sort) {
+            if ($Filter) {
+                $CombinedURL = -join ($CombinedURL, "`&`$orderby=$Sort")
+             } else {
+                $CombinedURL = -join ($CombinedURL, "`?`$orderby=$Sort")
+            }
+        }
+
+        if ($Paging) {
+            if ($Filter -or $Sort) {
+                $CombinedURL = -join ($CombinedURL, "`&`$$Paging")
+            } else {
+                $CombinedURL = -join ($CombinedURL, "`?`$$Paging")
+            }
+        }
+
+        $result = Get-RequestData -URI "$CombinedURL" -AuthString "Bearer $($VSAConnection.GetToken())"
+
+        return $result
+    }
+    else
+    { throw "Connection status: $ConnectionStatus" }
+}
+Export-ModuleMember -Function Get-VSAAgents

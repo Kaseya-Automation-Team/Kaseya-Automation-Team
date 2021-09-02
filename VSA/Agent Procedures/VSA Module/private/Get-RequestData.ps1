980f2acdb2edb1560d228aa1bb02166e719afeed
vlad <vladislav.semko@kaseya.com>
2021-09-02 19:32:10 +0300

    Update Get-RequestData.ps1
    Added error handling in case Invoke-Request fails

--- VSA/Agent Procedures/VSA Module/private/Get-RequestData.ps1
+++ VSA/Agent Procedures/VSA Module/private/Get-RequestData.ps1
@@ -25,13 +25,16 @@ function Get-RequestData
     }
     
     Log-Event -Msg "Executing call $Method : $URI" -Id 0010 -Type "Information"
-    $response = Invoke-RestMethod @requestParameters
-    if (0 -eq $response.ResponseCode) {
-        return $response.Result
-    } else {
-        Log-Event -Msg "$response.Error" -Id 0000 -Type "Error"
-        throw $response.Error
-    }
+    
+    try {
+            $response = Invoke-RestMethod @requestParameters -ErrorAction Stop
+            if (0 -eq $response.ResponseCode) {
+                return $response.Result
+            } else {
+                Log-Event -Msg "$response.Error" -Id 0000 -Type "Error"
+                throw $response.Error
+            }
+    } catch { throw $($_.Exception.Message) }
 }
 Export-ModuleMember -Function Get-RequestData
 #endregion function Get-RequestData

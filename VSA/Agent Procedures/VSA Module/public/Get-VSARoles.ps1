3504754195c11865d1103eb122e3d6af40882368
vlad <vladislav.semko@kaseya.com>
2021-09-07 19:58:05 +0300

    Update Get-VSARoles.ps1
    Rewritten to leverage Get-VSAItems

--- VSA/Agent Procedures/VSA Module/public/Get-VSARoles.ps1
+++ VSA/Agent Procedures/VSA Module/public/Get-VSARoles.ps1
@@ -1,53 +1,67 @@
 function Get-VSARoles
 {
-
+    <#
+    .Synopsis
+        Returns VSA roles.
+    .DESCRIPTION
+       Returns existing VSA roles.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER Filter
+        Specifies REST API Filter.
+    .PARAMETER Paging
+        Specifies REST API Paging.
+    .PARAMETER Sort
+        Specifies REST API Sorting.
+    .EXAMPLE
+       Get-VSARoles
+    .EXAMPLE
+       Get-VSARoles -VSAConnection $connection
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       Array of objects that represent existing VSA roles.
+    #>
     [CmdletBinding()]
     param ( 
-        [parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
         [VSAConnection] $VSAConnection,
-        [parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
         [ValidateNotNullOrEmpty()] 
-        [string] $SystemUsersSuffix = 'system/roles',
-        [parameter(Mandatory=$false)]
+        [string] $URISuffix = 'api/v1.0/system/roles',
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
         [ValidateNotNullOrEmpty()] 
         [string] $Filter,
-        [parameter(Mandatory=$false)]
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
         [ValidateNotNullOrEmpty()] 
         [string] $Paging,
-        [parameter(Mandatory=$false)]
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
         [ValidateNotNullOrEmpty()] 
         [string] $Sort
     )
 
-    if ( $($VSAConnection.GetStatus()) -eq "Open") #if token is valid
-    {
-        $CombinedURL = "$($VSAConnection.URI)/$SystemUsersSuffix"
-        
-        if ($Filter) {
-            $CombinedURL = -join ($CombinedURL, "`?`$filter=$Filter")
-        }
-
-        if ($Sort) {
-            if ($Filter) {
-                $CombinedURL = -join ($CombinedURL, "`&`$orderby=$Sort")
-             } else {
-                $CombinedURL = -join ($CombinedURL, "`?`$orderby=$Sort")
-            }
-        }
-
-        if ($Paging) {
-            if ($Filter -or $Sort) {
-                $CombinedURL = -join ($CombinedURL, "`&`$$Paging")
-            } else {
-                $CombinedURL = -join ($CombinedURL, "`?`$$Paging")
-            }
-        }
-
-        $result = Get-RequestData -URI "$CombinedURL" -AuthString "Bearer $($VSAConnection.GetToken())"
-
-        return $result
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
     }
-    else
-    { throw "Connection status: $ConnectionStatus" }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+    if($Filter)        {$Params.Add('Filter', $Filter)}
+    if($Paging)        {$Params.Add('Paging', $Paging)}
+    if($Sort)          {$Params.Add('Sort', $Sort)}
+
+    return Get-VSAItems @Params
 }
 Export-ModuleMember -Function Get-VSARoles

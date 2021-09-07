1025fbe01a874ddd0ddecf39d6aa1eef02935598
vlad <vladislav.semko@kaseya.com>
2021-09-07 12:01:24 +0300

    Update Get-VSAAgents.ps1
    Description added. Variables renamed. Persistent connection processing added

--- VSA/Agent Procedures/VSA Module/public/Get-VSAAgents.ps1
+++ VSA/Agent Procedures/VSA Module/public/Get-VSAAgents.ps1
@@ -1,53 +1,103 @@
 function Get-VSAAgents
 {
+    <#
+    .Synopsis
+       Returns VSA agents
+    .DESCRIPTION
+       Returns existing VSA agents.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER Filter
+        Specifies REST API Filter.
+    .PARAMETER Paging
+        Specifies REST API Paging.
+    .PARAMETER Sort
+        Specifies REST API Sorting.
+    .EXAMPLE
+       Get-VSAAgents
+    .EXAMPLE
+       Get-VSAAgents -VSAConnection $connection
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       Array of custom objects that represent existing VSA agents
+    #>
 
     [CmdletBinding()]
     param ( 
-        [parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
         [VSAConnection] $VSAConnection,
-        [parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
         [ValidateNotNullOrEmpty()] 
-        [string] $SystemUsersSuffix = 'assetmgmt/agents',
-        [parameter(Mandatory=$false)]
+        [string] $URISuffix = 'api/v1.0/assetmgmt/agents',
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
         [ValidateNotNullOrEmpty()] 
         [string] $Filter,
-        [parameter(Mandatory=$false)]
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
         [ValidateNotNullOrEmpty()] 
         [string] $Paging,
-        [parameter(Mandatory=$false)]
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
         [ValidateNotNullOrEmpty()] 
         [string] $Sort
     )
 
-    if ( $($VSAConnection.GetStatus()) -eq "Open") #if token is valid
+    if ([VSAConnection]::IsPersistent)
     {
-        $CombinedURL = "$($VSAConnection.URI)/$SystemUsersSuffix"
-        
-        if ($Filter) {
-            $CombinedURL = -join ($CombinedURL, "`?`$filter=$Filter")
-        }
-
-        if ($Sort) {
-            if ($Filter) {
-                $CombinedURL = -join ($CombinedURL, "`&`$orderby=$Sort")
-             } else {
-                $CombinedURL = -join ($CombinedURL, "`?`$orderby=$Sort")
-            }
-        }
-
-        if ($Paging) {
-            if ($Filter -or $Sort) {
-                $CombinedURL = -join ($CombinedURL, "`&`$$Paging")
-            } else {
-                $CombinedURL = -join ($CombinedURL, "`?`$$Paging")
-            }
-        }
-
-        $result = Get-RequestData -URI "$CombinedURL" -AuthString "Bearer $($VSAConnection.GetToken())"
-
-        return $result
+        $CombinedURL = "$([VSAConnection]::GetPersistentURI())/$URISuffix"
+        $UsersToken = "Bearer $( [VSAConnection]::GetPersistentToken() )"
     }
     else
-    { throw "Connection status: $ConnectionStatus" }
+    {
+        $ConnectionStatus = $VSAConnection.GetStatus()
+
+        if ( 'Open' -eq $ConnectionStatus )
+        {
+            $CombinedURL = "$($VSAConnection.URI)/$URISuffix"
+            $UsersToken = "Bearer $($VSAConnection.GetToken())"
+        }
+        else
+        {
+            throw "Connection status: $ConnectionStatus"
+        }
+    }
+
+    #region Filterin, Sorting, Paging
+    if ($Filter) {
+        $CombinedURL = -join ($CombinedURL, "`?`$filter=$Filter")
+    }
+
+    if ($Sort) {
+        if ($Filter) {
+            $CombinedURL = -join ($CombinedURL, "`&`$orderby=$Sort")
+            } else {
+            $CombinedURL = -join ($CombinedURL, "`?`$orderby=$Sort")
+        }
+    }
+
+    if ($Paging) {
+        if ($Filter -or $Sort) {
+            $CombinedURL = -join ($CombinedURL, "`&`$$Paging")
+        } else {
+            $CombinedURL = -join ($CombinedURL, "`?`$$Paging")
+        }
+    }
+    #endregion Filterin, Sorting, Paging
+
+    $result = Get-RequestData -URI $CombinedURL -AuthString $UsersToken
+
+    return $result
 }
 Export-ModuleMember -Function Get-VSAAgents

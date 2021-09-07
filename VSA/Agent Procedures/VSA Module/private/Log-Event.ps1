b6686935ffdfc4dcdf65a91d8d262d1802b770e0
vlad <vladislav.semko@kaseya.com>
2021-09-07 11:27:28 +0300

    Update Log-Event.ps1
    Validation added. Minor code changes

--- VSA/Agent Procedures/VSA Module/private/Log-Event.ps1
+++ VSA/Agent Procedures/VSA Module/private/Log-Event.ps1
@@ -1,24 +1,35 @@
 function Log-Event {
     <#
     .Synopsis
-       Logs the module's commandlets information.
+        Logs the module's commandlets informationin the Application event log.
     .DESCRIPTION
+        Creates entries in the Application event log that for the VSA Module commandlets.
+    .PARAMETER Msg
+        Message body.
+    .PARAMETER Id
+        The event Id.
+    .PARAMETER Type
+        Log entry type.
     #>
     param(        
-        [Parameter(Mandatory=$true)][String]$Msg,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Msg,
         [Parameter(Mandatory=$true)][Int]$Id,
-        [Parameter(Mandatory=$true)][String]$Type
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$true)]
+        [String]$Type
     )
 
     #Check if log source alread exists
-    $SourceExists = [System.Diagnostics.EventLog]::SourceExists("VSA API Module")
+    $SourceExists = try { [System.Diagnostics.EventLog]::SourceExists("VSA API Module") } catch {$false}
 
     #If not, create a new one
-    if ($SourceExists -eq $false) {
-        New-EventLog –LogName Application –Source “VSA API Module”
+    if ( -Not $SourceExists ) {
+        New-EventLog -LogName Application -Source "VSA API Module"
     }
 
-    Write-EventLog –LogName Application –Source “VSA API Module” –EntryType $Type –EventID $Id  –Message $Msg -Category 0
-    $CurrentTime = Get-Date
-    Write-Host "$CurrentTime`: $Type`: $Msg"
+    Write-EventLog -LogName Application -Source "VSA API Module" -EntryType $Type -EventID $Id -Message $Msg -Category 0
+    #$CurrentTime = Get-Date
+    #Write-Host "$CurrentTime`: $Type`: $Msg"
 }

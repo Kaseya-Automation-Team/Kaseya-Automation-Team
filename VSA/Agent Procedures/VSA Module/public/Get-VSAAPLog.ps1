cb36c468308faf8f9c3944dbeb50e2f8e3ba533a
vlad <vladislav.semko@kaseya.com>
2021-09-07 19:42:35 +0300

    Update Get-VSAAPLog.ps1
    Rewritten to leverage Get-VSAItems

--- VSA/Agent Procedures/VSA Module/public/Get-VSAAPLog.ps1
+++ VSA/Agent Procedures/VSA Module/public/Get-VSAAPLog.ps1
@@ -1,58 +1,75 @@
 function Get-VSAAPLog
 {
-
+    <#
+    .Synopsis
+       Returns VSA agent procedures' log.
+    .DESCRIPTION
+       Returns the VSA agent procedures' for given Agent Id.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER AgentId
+        Specifies Agent Id to return the agent procedures' log.
+    .PARAMETER Filter
+        Specifies REST API Filter.
+    .PARAMETER Paging
+        Specifies REST API Paging.
+    .PARAMETER Sort
+        Specifies REST API Sorting.
+    .EXAMPLE
+       Get-VSAAPLog -AgentId '00001'
+    .EXAMPLE
+       Get-VSAAPLog -VSAConnection $connection -AgentId '00001'
+    .INPUTS
+       Accepts piped non-persistent VSAConnection
+    .OUTPUTS
+       Array of objects that represent the VSA VSA agent procedures' log
+    #>
     [CmdletBinding()]
     param ( 
-        [parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
         [VSAConnection] $VSAConnection,
-        [parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
         [ValidateNotNullOrEmpty()] 
-        [PSObject] $AgentId,
-		[parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [string] $URISuffix = "api/v1.0/assetmgmt/logs/{0}/agentprocedure",
+        [parameter(ParameterSetName = 'Persistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(ParameterSetName = 'NonPersistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
         [ValidateNotNullOrEmpty()] 
-        [string] $SystemUsersSuffix = "/assetmgmt/logs/{agentid}/agentprocedure",
-        [parameter(Mandatory=$false)]
+        [string] $AgentId,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
         [ValidateNotNullOrEmpty()] 
         [string] $Filter,
-        [parameter(Mandatory=$false)]
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
         [ValidateNotNullOrEmpty()] 
         [string] $Paging,
-        [parameter(Mandatory=$false)]
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
         [ValidateNotNullOrEmpty()] 
         [string] $Sort
     )
 
-    $SystemUsersSuffix = $SystemUsersSuffix -replace "{agentid}", $AgentId
+    $URISuffix = $URISuffix -f $AgentId
 
-    if ( $($VSAConnection.GetStatus()) -eq "Open") #if token is valid
-    {
-        $CombinedURL = "$($VSAConnection.URI)/$SystemUsersSuffix"
-        
-        if ($Filter) {
-            $CombinedURL = -join ($CombinedURL, "`?`$filter=$Filter")
-        }
-
-        if ($Sort) {
-            if ($Filter) {
-                $CombinedURL = -join ($CombinedURL, "`&`$orderby=$Sort")
-             } else {
-                $CombinedURL = -join ($CombinedURL, "`?`$orderby=$Sort")
-            }
-        }
-
-        if ($Paging) {
-            if ($Filter -or $Sort) {
-                $CombinedURL = -join ($CombinedURL, "`&`$$Paging")
-            } else {
-                $CombinedURL = -join ($CombinedURL, "`?`$$Paging")
-            }
-        }
-
-        $result = Get-RequestData -URI "$CombinedURL" -AuthString "Bearer $($VSAConnection.GetToken())"
-
-        return $result
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
     }
-    else
-    { throw "Connection status: $ConnectionStatus" }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+    if($Filter)        {$Params.Add('Filter', $Filter)}
+    if($Paging)        {$Params.Add('Paging', $Paging)}
+    if($Sort)          {$Params.Add('Sort', $Sort)}
+
+    return Get-VSAItems @Params
 }
 Export-ModuleMember -Function Get-VSAAPLog

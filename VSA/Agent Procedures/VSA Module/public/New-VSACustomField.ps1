7bc70f1a258598eff5397665983c3331af7b1e13
vlad <vladislav.semko@kaseya.com>
2021-09-09 20:10:53 +0300

    Create New-VSACustomField.ps1
    Demo. Creating new objects with POST method

---
+++ VSA/Agent Procedures/VSA Module/public/New-VSACustomField.ps1
@@ -0,0 +1,41 @@
+﻿function New-VSACustomField {
+    $URISuffix = 'api/v1.0/assetmgmt/assets/customfields'
+
+    if ([VSAConnection]::IsPersistent)
+    {
+        $CombinedURL = "$([VSAConnection]::GetPersistentURI())/$URISuffix"
+        $UsersToken = "Bearer $( [VSAConnection]::GetPersistentToken() )"
+    }
+    else
+    {
+        $ConnectionStatus = $VSAConnection.GetStatus()
+
+        if ( 'Open' -eq $ConnectionStatus )
+        {
+            $CombinedURL = "$($VSAConnection.URI)/$URISuffix"
+            $UsersToken = "Bearer $($VSAConnection.GetToken())"
+        }
+        else
+        {
+            throw "Connection status: $ConnectionStatus"
+        }
+    }
+    $Body = @(@{"key"="FieldName";"value"="TestField"},@{ "key"="FieldType";"value"="string" }) | ConvertTo-Json
+
+    $authHeader = @{
+        Authorization = $UsersToken
+    }
+
+    $requestParameters = @{
+        Uri = $CombinedURL
+        Method = 'Post'
+        Headers = $authHeader
+        Body = $Body
+        ContentType = "application/json"
+        Verbose = $true
+    }
+    $requestParameters
+
+    Invoke-WebRequest @requestParameters
+}
+Export-ModuleMember -Function New-VSACustomField

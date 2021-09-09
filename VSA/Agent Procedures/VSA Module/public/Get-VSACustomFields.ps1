639e68ae5f22b82d70d163b1eb2554147168ca8d
vlad <vladislav.semko@kaseya.com>
2021-09-09 20:12:03 +0300

    Create Get-VSACustomFields.ps1

---
+++ VSA/Agent Procedures/VSA Module/public/Get-VSACustomFields.ps1
@@ -0,0 +1,67 @@
+function Get-VSACustomFields
+{
+<#
+.Synopsis
+   Returns VSA custom fields.
+.DESCRIPTION
+   Returns existing VSA custom fields.
+   Takes either persistent or non-persistent connection information.
+.PARAMETER VSAConnection
+    Specifies existing non-persistent VSAConnection.
+.PARAMETER URISuffix
+    Specifies URI suffix if it differs from the default.
+.PARAMETER Filter
+    Specifies REST API Filter.
+.PARAMETER Paging
+    Specifies REST API Paging.
+.PARAMETER Sort
+    Specifies REST API Sorting.
+.EXAMPLE
+   Get-VSACustomFields
+.EXAMPLE
+   Get-VSACustomFields -VSAConnection $connection
+.INPUTS
+   Accepts piped non-persistent VSAConnection 
+.OUTPUTS
+   Array of objects that represent existing VSA users
+#>
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
+        [VSAConnection] $VSAConnection,
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = 'api/v1.0/assetmgmt/assets/customfields',
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Filter,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Paging,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Sort
+    )
+
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+    if($Filter)        {$Params.Add('Filter', $Filter)}
+    if($Paging)        {$Params.Add('Paging', $Paging)}
+    if($Sort)          {$Params.Add('Sort', $Sort)}
+
+    return Get-VSAItems @Params
+}
+Export-ModuleMember -Function Get-VSACustomFields

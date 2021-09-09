fa6ddd31d003db6600cbdabacde3443f6d598c84
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-09-09 14:37:10 +0300

    Add comments to New-VSAToken and enable info log messages

--- VSA/Agent Procedures/VSA Module/public/New-VSAToken.ps1
+++ VSA/Agent Procedures/VSA Module/public/New-VSAToken.ps1
@@ -1,8 +1,24 @@
 ﻿function New-VSAToken
 {
+<#
+.Synopsis
+   Generates authentication file
+.DESCRIPTION
+   Saves encoded API credentials into text file
+.EXAMPLE
+   New-VSAToken
+.OUTPUTS
+   Information messages indicating success/failure
+#>
     $creds = Get-Credential -Message "Enter your API username and Personal Authentication Token"
     
-    [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("$($creds.UserName):$($creds.Password | Convertfrom-SecureString)")) | Out-File "$PSScriptRoot\..\private\pat.txt"
+    try {
+        [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("$($creds.UserName):$($creds.Password | Convertfrom-SecureString)")) | Out-File "$PSScriptRoot\..\private\pat.txt"
+        Log-Event -Msg "New authentication pair have been generated and saved in $PSScriptRoot\..\private\pat.txt" -Id 9999 -Type "Information"
+    }
+    catch {
+        
+    }
 }
 
 Export-ModuleMember -Function New-VSAToken

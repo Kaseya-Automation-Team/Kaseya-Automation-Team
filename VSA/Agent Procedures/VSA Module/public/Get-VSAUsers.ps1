05dee01adb996a65abe1dabf465176c657368479
vlad <vladislav.semko@kaseya.com>
2021-09-10 13:47:09 +0300

    Update Get-VSAUsers.ps1
    Added ID decoding for roles & scopes

--- VSA/Agent Procedures/VSA Module/public/Get-VSAUsers.ps1
+++ VSA/Agent Procedures/VSA Module/public/Get-VSAUsers.ps1
@@ -1,30 +1,32 @@
 function Get-VSAUsers
 {
-<#
-.Synopsis
-   Returns VSA users
-.DESCRIPTION
-   Returns existing VSA users.
-   Takes either persistent or non-persistent connection information.
-.PARAMETER VSAConnection
-    Specifies existing non-persistent VSAConnection.
-.PARAMETER URISuffix
-    Specifies URI suffix if it differs from the default.
-.PARAMETER Filter
-    Specifies REST API Filter.
-.PARAMETER Paging
-    Specifies REST API Paging.
-.PARAMETER Sort
-    Specifies REST API Sorting.
-.EXAMPLE
-   Get-VSAUsers
-.EXAMPLE
-   Get-VSAUsers -VSAConnection $connection
-.INPUTS
-   Accepts piped non-persistent VSAConnection 
-.OUTPUTS
-   Array of objects that represent existing VSA users
-#>
+    <#
+    .Synopsis
+       Returns VSA users
+    .DESCRIPTION
+       Returns existing VSA users.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER Filter
+        Specifies REST API Filter.
+    .PARAMETER Paging
+        Specifies REST API Paging.
+    .PARAMETER Sort
+        Specifies REST API Sorting.
+    .PARAMETER ResolveIDs
+        Return Roles & Scopes as well as their respective IDs.
+    .EXAMPLE
+       Get-VSAUsers
+    .EXAMPLE
+       Get-VSAUsers -VSAConnection $connection
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       Array of objects that represent existing VSA users
+    #>
     [CmdletBinding()]
     param ( 
         [parameter(Mandatory = $true, 
@@ -50,7 +52,10 @@ function Get-VSAUsers
         [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
         [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
         [ValidateNotNullOrEmpty()] 
-        [string] $Sort
+        [string] $Sort,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [switch] $ResolveIDs
     )
 
     [hashtable]$Params =@{
@@ -62,6 +67,32 @@ function Get-VSAUsers
     if($Paging)        {$Params.Add('Paging', $Paging)}
     if($Sort)          {$Params.Add('Sort', $Sort)}
 
-    return Get-VSAItems @Params
+    $result = Get-VSAItems @Params
+
+    if ($ResolveIDs)
+    {
+        [hashtable]$ResolveParams =@{}
+        if($VSAConnection) {$ResolveParams.Add('VSAConnection', $VSAConnection)}
+
+        [hashtable]$RolesDictionary = @{}
+        [hashtable]$ScopesDictionary = @{}
+
+        Foreach( $Role in $(Get-VSAUserRoles @ResolveParams) )
+        {
+            if ( -Not $RolesDictionary[$Role.RoleId]){}
+            $RolesDictionary.Add($Role.RoleId, $($Role | Select-Object * -ExcludeProperty RoleId))
+        }
+
+        Foreach( $Scope in $(Get-VSAScopes @ResolveParams) )
+        {
+            if ( -Not $ScopesDictionary[$Scope.ScopeId]){}
+            $ScopesDictionary.Add($Scope.ScopeId, $Scope.ScopeName)
+        }
+        $result = $result | Select-Object -Property *, `
+            @{Name = 'AdminRoles'; Expression = { $RolesDictionary[$_.AdminRoleIds] }},
+            @{Name = 'AdminScopes'; Expression = { $ScopesDictionary[$_.AdminScopeIds] }}
+    }
+
+    return $result
 }
 Export-ModuleMember -Function Get-VSAUsers

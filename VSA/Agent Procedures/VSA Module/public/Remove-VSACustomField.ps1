71cdbea6b243c7bc7d36723446a83c5fe18900e8
vlad <vladislav.semko@kaseya.com>
2021-09-10 18:43:37 +0300

    Create Remove-VSACustomField.ps1

---
+++ VSA/Agent Procedures/VSA Module/public/Remove-VSACustomField.ps1
@@ -0,0 +1,66 @@
+﻿function Remove-VSACustomField {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
+        [VSAConnection] $VSAConnection,
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
+        [ValidateNotNullOrEmpty()]
+        [string] $URISuffix = 'api/v1.0//assetmgmt/assets/customfields/{0}',
+        [parameter(Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
+        [ValidateNotNullOrEmpty()] 
+        [string] $FieldName
+        )
+
+    if ([VSAConnection]::IsPersistent)
+    {
+        $CombinedURL = "$([VSAConnection]::GetPersistentURI())/$URISuffix"
+        $UsersToken = "Bearer $( [VSAConnection]::GetPersistentToken() )"
+    }
+    else
+    {
+        $ConnectionStatus = $VSAConnection.GetStatus()
+
+        if ( 'Open' -eq $ConnectionStatus )
+        {
+            $CombinedURL = "$($VSAConnection.URI)/$URISuffix"
+            $UsersToken = "Bearer $($VSAConnection.GetToken())"
+        }
+        else
+        {
+            throw "Connection status: $ConnectionStatus"
+        }
+    }
+
+    $URISuffix = $URISuffix -f $FieldName
+
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+    
+    $authHeader = @{
+        Authorization = $UsersToken
+    }
+
+    $requestParameters = @{
+        Uri = $CombinedURL
+        Method = 'DELETE'
+        Headers = $authHeader
+    }
+
+    Get-RequestData @requestParameters
+}
+Export-ModuleMember -Function Remove-VSACustomField

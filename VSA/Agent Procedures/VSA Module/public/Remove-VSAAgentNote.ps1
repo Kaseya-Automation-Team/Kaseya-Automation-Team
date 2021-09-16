3b882cbf2e94879b0cd79c240ecba0c9751c599a
vlad <vladislav.semko@kaseya.com>
2021-09-16 15:13:25 +0300

    Create Remove-VSAAgentNote.ps1

---
+++ VSA/Agent Procedures/VSA Module/public/Remove-VSAAgentNote.ps1
@@ -0,0 +1,60 @@
+﻿function Remove-VSAAgentNote {
+    <#
+    .Synopsis
+       Removes an agent note.
+    .DESCRIPTION
+       Removes an agent note.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER NoteId
+        Id of note that is changed.
+    .EXAMPLE
+       Remove-VSAAgentNote -NoteId '1'
+    .EXAMPLE
+       Remove-VSAAgentNote -NoteId '1' -VSAConnection $connection
+    .INPUTS
+       Accepts piped non-persistent VSAConnection
+    .OUTPUTS
+       True if removing was successful.
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNull()]
+        [VSAConnection] $VSAConnection,
+
+        [parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = 'api/v1.0/assetmgmt/agent/note/{0}',
+
+        [Parameter(Mandatory = $true)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string] $NoteId
+        )
+
+    $URISuffix = $URISuffix -f $NoteId
+    [bool]$result = $false
+
+
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
+        Method = 'DELETE'
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+
+    If ( $NoteId -in $(Get-VSAAgentNote | Select-Object -ExpandProperty ID) ) {
+        $result = Update-VSAItems @Params
+    } else {
+        $Message = "The agent note with ID `'$NoteId`' does not exist"
+        Log-Event -Msg $Message -Id 4000 -Type "Error"
+        throw $Message
+    }
+    return $result
+}
+Export-ModuleMember -Function Remove-VSAAgentNote

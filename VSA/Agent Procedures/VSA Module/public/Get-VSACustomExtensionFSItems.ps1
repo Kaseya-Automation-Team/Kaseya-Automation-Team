db67b23648c255bff403806830b5bd3158f13ada
vlad <vladislav.semko@kaseya.com>
2021-09-16 19:37:32 +0300

    Create Get-VSACustomExtensionFSItems.ps1

---
+++ VSA/Agent Procedures/VSA Module/public/Get-VSACustomExtensionFSItems.ps1
@@ -0,0 +1,66 @@
+﻿
+function Get-VSACustomExtensionFSItems
+{
+    <#
+    .Synopsis
+
+    #>
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNull()]
+        [VSAConnection] $VSAConnection,
+
+        [parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = 'api/v1.0/assetmgmt/customextensions/{0}/folder/{1}',
+
+        [Parameter(Mandatory = $true)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string] $AgentId,
+
+        [Parameter(Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Path = '/',
+
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Filter,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Paging,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Sort
+    )
+
+    $URISuffix = $URISuffix -f $AgentId, $Path
+
+    [hashtable]$Params = @{}
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+
+    If ( $AgentId -in $(Get-VSAAgents @Params | Select-Object -ExpandProperty AgentID) ) {
+
+        $Params.Add('URISuffix', $URISuffix)
+        if($Filter)        {$Params.Add('Filter', $Filter)}
+        if($Paging)        {$Params.Add('Paging', $Paging)}
+        if($Sort)          {$Params.Add('Sort', $Sort)}
+
+        $Params | Out-String | Write-Verbose
+        $Params | Out-String | Write-Debug
+
+        $result = Get-VSAItems @Params
+
+    } else {
+        $Message = "The asset with Agent ID `'$AgentId`' does not exist"
+        Log-Event -Msg $Message -Id 4000 -Type "Error"
+        throw $Message
+    }
+
+}
+Export-ModuleMember -Function Get-VSACustomExtensionFSItems

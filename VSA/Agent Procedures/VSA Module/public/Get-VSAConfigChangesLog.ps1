745815825867f99a6bc1200e85992c13a007357e
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-09-16 18:30:12 +0300

    Add new methods

---
+++ VSA/Agent Procedures/VSA Module/public/Get-VSAConfigChangesLog.ps1
@@ -0,0 +1,74 @@
+function Get-VSAConfigChangesLog {
+    <#
+    .Synopsis
+       Returns VSA configuration changes log.
+    .DESCRIPTION
+       Returns the VSA configuration changes for given Agent Id.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER AgentId
+        Specifies Agent Id to return the agent procedures' log.
+    .PARAMETER Filter
+        Specifies REST API Filter.
+    .PARAMETER Paging
+        Specifies REST API Paging.
+    .PARAMETER Sort
+        Specifies REST API Sorting.
+    .EXAMPLE
+       Get-VSAConfigChangesLog -AgentId '00001'
+    .EXAMPLE
+       Get-VSAConfigChangesLog -VSAConnection $connection -AgentId '00001'
+    .INPUTS
+       Accepts piped non-persistent VSAConnection
+    .OUTPUTS
+       Array of objects that represent the VSA alarm log
+    #>
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
+        [VSAConnection] $VSAConnection,
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = "api/v1.0/assetmgmt/logs/{0}/configurationchanges",
+        [parameter(ParameterSetName = 'Persistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(ParameterSetName = 'NonPersistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $AgentId,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Filter,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Paging,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Sort
+    )
+
+    $URISuffix = $URISuffix -f $AgentId
+
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+    if($Filter)        {$Params.Add('Filter', $Filter)}
+    if($Paging)        {$Params.Add('Paging', $Paging)}
+    if($Sort)          {$Params.Add('Sort', $Sort)}
+
+    return Get-VSAItems @Params
+}
+Export-ModuleMember -Function Get-VSAConfigChangesLog

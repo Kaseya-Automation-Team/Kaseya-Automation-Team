e61efcf76492339cd36aa195e47f6b643f741cd7
vlad <vladislav.semko@kaseya.com>
2021-09-20 19:07:16 +0300

    Create Remove-VSACustomExtensionFolder.ps1

---
+++ VSA/Agent Procedures/VSA Module/public/Remove-VSACustomExtensionFolder.ps1
@@ -0,0 +1,79 @@
+﻿function Remove-VSACustomExtensionFolder
+{
+    <#
+    .Synopsis
+       Creates Custom Extension Folder.
+    .DESCRIPTION
+       Creates Custom Extension Folder.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER Folder
+        Specifies Relative agent's path.
+    .EXAMPLE
+       Remove-VSACustomExtensionFolder -AgentId '10001' -Folder 'NewFolder'
+    .EXAMPLE
+       Remove-VSACustomExtensionFolder -AgentId '10001' -Folder '/NewFolder1/NewFolder2/'
+    .EXAMPLE
+       Remove-VSACustomExtensionFolder -AgentId '10001' -Folder 'NewFolder3' -VSAConnection $connection
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       Array of objects that represent Custom Extension Folders and Files.
+    #>
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNull()]
+        [VSAConnection] $VSAConnection,
+
+        [parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = 'api/v1.0/assetmgmt/customextensions/{0}/{1}',
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $AgentId,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({
+            if( $_ -notmatch "^\w+"  ){
+                throw "Enter folder name"
+            }
+            return $true
+        })]
+        [string] $Folder
+    )
+
+    $Folder = "//$Folder"
+    $Folder = $Folder -replace '\\', '/'
+
+    $URISuffix = $URISuffix -f $AgentId, $Folder
+
+    [hashtable]$Params = @{}
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+
+    If ( $AgentId -in $(Get-VSAAgents @Params | Select-Object -ExpandProperty AgentID) ) {
+
+        $Params.Add('URISuffix', $URISuffix)
+        $Params.Add('Method', 'DELETE')
+
+        $Params | Out-String | Write-Verbose
+        $Params | Out-String | Write-Debug
+
+        $result = Update-VSAItems @Params
+
+        #No response for REST API call
+
+    } else {
+        $Message = "The asset with Agent ID `'$AgentId`' does not exist"
+        Log-Event -Msg $Message -Id 4000 -Type "Error"
+        throw $Message
+    }
+
+    return $result
+}
+Export-ModuleMember -Function Remove-VSACustomExtensionFolder

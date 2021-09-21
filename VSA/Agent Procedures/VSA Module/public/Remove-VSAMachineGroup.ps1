d04b9f4776f97d32a69bd3dac456daad30197a89
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-09-21 20:25:52 +0300

    Remove machine group function

---
+++ VSA/Agent Procedures/VSA Module/public/Remove-VSAMachineGroup.ps1
@@ -0,0 +1,56 @@
+function Remove-VSAMachineGroup
+{
+    <#
+    .Synopsis
+       Removes machine group
+    .DESCRIPTION
+       Removes specified VSA machine group.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER MachineGroupId
+        Specifies numeric id of machine group
+    .EXAMPLE
+       Remove-VSAMachineGroup -MachineGroupId "34543554343"
+    .EXAMPLE
+       Remove-VSAMachineGroup -VSAConnection $connection
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       No output
+    #>
+
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
+        [VSAConnection] $VSAConnection,
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = "api/v1.0/system/machinegroups/{0}",
+        [parameter(ParameterSetName = 'Persistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(ParameterSetName = 'NonPersistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $MachineGroupId
+)
+    $URISuffix = $URISuffix -f $MachineGroupId
+	
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
+        Method = 'DELETE'
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+
+    return Update-VSAItems @Params
+}
+
+Export-ModuleMember -Function Remove-VSAMachineGroup

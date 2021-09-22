a5fbed7964cb4bbfdb0344df62aa6c2f574ae199
vlad <vladislav.semko@kaseya.com>
2021-09-22 16:19:12 +0300

    Create Get-VSARoleType.ps1
    Renamed Get-VSARoleTypes to Get-VSARoleType

---
+++ VSA/Agent Procedures/VSA Module/public/Get-VSARoleType.ps1
@@ -0,0 +1,71 @@
+function Get-VSARoleType
+{
+    <#
+    .Synopsis
+       Returns VSA user role types.
+    .DESCRIPTION
+       Returns existing VSA user role types.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER Filter
+        Specifies REST API Filter.
+    .PARAMETER Paging
+        Specifies REST API Paging.
+    .PARAMETER Sort
+        Specifies REST API Sorting.
+    .EXAMPLE
+       Get-VSARoleType
+    .EXAMPLE
+       Get-VSARoleType -VSAConnection $connection
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       Array of objects that represent existing VSA user role types.
+    #>
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
+        [VSAConnection] $VSAConnection,
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = 'api/v1.0/system/roletypes',
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Filter,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Paging,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Sort,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [switch] $ResolveIDs
+
+    )
+
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+    if($Filter)        {$Params.Add('Filter', $Filter)}
+    if($Paging)        {$Params.Add('Paging', $Paging)}
+    if($Sort)          {$Params.Add('Sort', $Sort)}
+
+    return Get-VSAItems @Params
+}
+Export-ModuleMember -Function Get-VSARoleType

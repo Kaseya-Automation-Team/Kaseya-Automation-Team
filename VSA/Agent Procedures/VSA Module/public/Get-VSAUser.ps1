b58c9ebf13ae5f04a161139fcc5dbb9b1a2b4fee
vlad <vladislav.semko@kaseya.com>
2021-09-24 13:53:04 +0300

    Create Get-VSAUser.ps1
    Get-VSAUser

---
+++ VSA/Agent Procedures/VSA Module/public/Get-VSAUser.ps1
@@ -0,0 +1,120 @@
+function Get-VSAUser
+{
+    <#
+    .Synopsis
+       Returns VSA users
+    .DESCRIPTION
+       Returns existing VSA users.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER UserId
+        Specifies User Id. Returns all users if no User ID specified
+    .PARAMETER Filter
+        Specifies REST API Filter.
+    .PARAMETER Paging
+        Specifies REST API Paging.
+    .PARAMETER Sort
+        Specifies REST API Sorting.
+    .PARAMETER ResolveIDs
+        Return Roles & Scopes as well as their respective IDs.
+    .EXAMPLE
+       Get-VSAUser
+    .EXAMPLE
+       Get-VSAUser -VSAConnection $connection
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       Array of objects that represent existing VSA users
+    #>
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
+        [VSAConnection] $VSAConnection,
+
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = 'api/v1.0/system/users',
+
+        [Parameter(Mandatory = $false)]
+        [ValidateScript({
+            if( $_ -notmatch "^\d+$" ) {
+                throw "Non-numeric Id"
+            }
+            return $true
+        })]
+        [string] $UserId,
+
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Filter,
+
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Paging,
+
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Sort,
+
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [switch] $ResolveIDs
+    )
+
+    if( -not [string]::IsNullOrWhiteSpace($UserId) ) {
+        $URISuffix += "/$UserId"
+    }
+
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+    if($Filter)        {$Params.Add('Filter', $Filter)}
+    if($Paging)        {$Params.Add('Paging', $Paging)}
+    if($Sort)          {$Params.Add('Sort', $Sort)}
+
+    $result = Get-VSAItems @Params
+
+    if ($ResolveIDs)
+    {
+        [hashtable]$ResolveParams =@{}
+        if($VSAConnection) {$ResolveParams.Add('VSAConnection', $VSAConnection)}
+
+        [hashtable]$RolesDictionary = @{}
+        [hashtable]$ScopesDictionary = @{}
+
+        Foreach( $Role in $(Get-VSARoles @ResolveParams -ResolveIDs) )
+        {
+            if ( -Not $RolesDictionary[$Role.RoleId]){
+                $RolesDictionary.Add($Role.RoleId, $($Role | Select-Object * -ExcludeProperty RoleId))
+            }
+        }
+
+        Foreach( $Scope in $(Get-VSAScope @ResolveParams) )
+        {
+            if ( -Not $ScopesDictionary[$Scope.ScopeId]){
+                $ScopesDictionary.Add($Scope.ScopeId, $Scope.ScopeName)
+            }
+        }
+        $result = $result | Select-Object -Property *, `
+            @{Name = 'AdminRoles'; Expression = { $RolesDictionary[$_.AdminRoleIds] }},
+            @{Name = 'AdminScopes'; Expression = { $ScopesDictionary[$_.AdminScopeIds] }}
+    }
+
+    return $result
+}
+Export-ModuleMember -Function Get-VSAUser

b7ac366af414d3206f94e02f3827f31c3e09c55e
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-09-27 16:10:01 +0300

    New AP functions

---
+++ VSA/Agent Procedures/VSA Module/public/Start-VSAAP.ps1
@@ -0,0 +1,63 @@
+function Start-VSAAP
+{
+    <#
+    .Synopsis
+       Runs agent procedure
+    .DESCRIPTION
+       Runs an agent procedure immediately for a single agent.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER AgentId
+        Specifies id of the agent machine
+    .PARAMETER AgentProcedureId
+        Specifies id of agent procedire
+    .EXAMPLE
+       Start-VSAAP -AgentId 34223222 -AgentProcedureId 2312
+    .EXAMPLE
+       Start-VSAAP -VSAConnection $connection -AgentId 34223222 -AgentProcedureId 2312
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       No output
+    #>
+
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
+        [VSAConnection] $VSAConnection,
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = "api/v1.0/automation/agentprocs/{0}/{1}/runnow",
+        [parameter(ParameterSetName = 'Persistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(ParameterSetName = 'NonPersistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $AgentId,
+        [parameter(ParameterSetName = 'Persistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(ParameterSetName = 'NonPersistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $AgentProcedureId
+)
+    
+    $URISuffix = $URISuffix -f $AgentId, $AgentProcedureId
+    
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
+        Method = 'PUT'
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+
+    return Update-VSAItems @Params
+}
+
+Export-ModuleMember -Function Start-VSAAP

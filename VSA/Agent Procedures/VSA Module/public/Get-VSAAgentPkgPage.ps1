a56fbe05ab6cd759ff688032cfb83f096fcf8d69
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-10-05 13:53:11 +0300

    New functions

---
+++ VSA/Agent Procedures/VSA Module/public/Get-VSAAgentPkgPage.ps1
@@ -0,0 +1,76 @@
+function Get-VSAAgentPkgPage {
+    <#
+    .Synopsis
+       Returns data from deployment page
+    .DESCRIPTION
+       Returns the logo, title and description displayed by the deployment page for an specified tenant partition.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER PartitionId
+        Specifies id of the tenant.
+    .PARAMETER Filter
+        Specifies REST API Filter.
+    .PARAMETER Paging
+        Specifies REST API Paging.
+    .PARAMETER Sort
+        Specifies REST API Sorting.
+    .EXAMPLE
+       Get-VSAAgentPkgPage
+    .EXAMPLE
+       Get-VSAAgentPkgPage -VSAConnection $connection
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       Array of items that represent deployment page data
+    #>
+
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
+        [VSAConnection] $VSAConnection,
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = 'api/v1.0/agent/{0}/deploypagecustomization',
+        [parameter(ParameterSetName = 'Persistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(ParameterSetName = 'NonPersistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $PartitionId,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Filter,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Paging,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Sort
+    )
+
+    $URISuffix = $URISuffix -f $PartitionId
+
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+    if($Filter)        {$Params.Add('Filter', $Filter)}
+    if($Paging)        {$Params.Add('Paging', $Paging)}
+    if($Sort)          {$Params.Add('Sort', $Sort)}
+
+    return Get-VSAItems @Params
+}
+
+Export-ModuleMember -Function Get-VSAAgentPkgPage

5171c937180967c93cbf5893baf93dd996e9f31a
vlad <vladislav.semko@kaseya.com>
2021-10-05 13:25:38 +0300

    Update Get-VSACustomExtensionFSItems.ps1
    Replace Get-VSAAgents with Get-VSAAgent

--- VSA/Agent Procedures/VSA Module/public/Get-VSACustomExtensionFSItems.ps1
+++ VSA/Agent Procedures/VSA Module/public/Get-VSACustomExtensionFSItems.ps1
@@ -71,7 +71,7 @@
     [hashtable]$Params = @{}
     if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
 
-    If ( $AgentId -in $(Get-VSAAgents @Params | Select-Object -ExpandProperty AgentID) ) {
+    If ( $AgentId -in $(Get-VSAAgent @Params | Select-Object -ExpandProperty AgentID) ) {
 
         $Params.Add('URISuffix', $URISuffix)
         if($Filter)        {$Params.Add('Filter', $Filter)}

1f9d6e77b36a1b41071b6de95bd3ccfff7f1f1e5
SarathMopuri-Kaseya <sarath.mopuri@kaseya.com>
2021-10-08 11:42:19 +0530

    Create Get-Installed-KB.ps1

---
+++ Public/Get-Installed-KB.ps1
@@ -0,0 +1,16 @@
+﻿$kb = $args[0]
+$workDir = $args[1]
+
+IF ($kb.Contains("KB"))
+{ 
+    $kb = $kb.Replace("KB","") 
+}
+
+$kb = 'KB' + $kb
+
+if ($kb -eq (get-hotfix | Where-Object HotFixID -Like $kb | Select-Object HotFixID | ft -hidetableheaders | Out-String).trim())
+{
+    $date = (get-hotfix | Where-Object HotFixID -Like $kb | Select-Object InstalledOn | ft -hidetableheaders | Out-String).trim()
+    
+    echo "$kb was installed on $date" | Out-File $workdir\$kb.txt
+}

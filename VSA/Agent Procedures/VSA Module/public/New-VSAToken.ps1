1cd71076b670e361e6ee6ad7d9b0771dfc8924e2
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2021-10-12 17:34:02 +0200

    Update New-VSAToken.ps1

--- VSA/Agent Procedures/VSA Module/public/New-VSAToken.ps1
+++ VSA/Agent Procedures/VSA Module/public/New-VSAToken.ps1
@@ -13,7 +13,7 @@
     $creds = Get-Credential -Message "Enter your API username and Personal Authentication Token"
     
     try {
-        [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("$($creds.UserName):$($creds.Password | Convertfrom-SecureString)")) | Out-File "$PSScriptRoot\..\private\pat.txt"
+        [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("$($creds.UserName):$($creds.Password | Convertfrom-SecureString)")) | Out-File "$PSScriptRoot\..\private\pat.txt" -Force
         Log-Event -Msg "New authentication pair have been generated and saved in $PSScriptRoot\..\private\pat.txt" -Id 9999 -Type "Information"
     }
     catch {

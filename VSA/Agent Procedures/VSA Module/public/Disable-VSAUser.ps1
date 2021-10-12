b4a6296c627bf8e16ab8317158c02d21a07d698f
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2021-10-12 15:36:19 +0200

    Create Disable-VSAUser.ps1

---
+++ VSA/Agent Procedures/VSA Module/public/Disable-VSAUser.ps1
@@ -0,0 +1,96 @@
+function Disable-VSAUser
+{
+    <#
+    .Synopsis
+       Disables a single user account record.
+    .DESCRIPTION
+       Disables a single user account record.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER UserId
+        Specifies a user account Id.
+    .PARAMETER AdminName
+        Specifies a user account name.
+    .EXAMPLE
+       Disable-VSAUser UserId 10001
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       True if addition was successful.
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'ById')]
+    #[CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'ByName')]
+        [parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'ById')]
+        [ValidateNotNull()]
+        [VSAConnection] $VSAConnection,
+
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'ByName')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'ById')]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = 'api/v1.0/system/users/{0}/disable',
+
+        [parameter(Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'ById')]
+        [decimal] $UserId
+    )
+
+    DynamicParam {
+
+            $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+            
+            [hashtable] $AuxParameters = @{}
+            if($VSAConnection) {$AuxParameters.Add('VSAConnection', $VSAConnection)}
+
+            [array] $script:Users = Get-VSAUser @AuxParameters | Select UserId, AdminName
+
+            $ParameterName = 'AdminName' 
+            $AttributesCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+            $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $ParameterAttribute.Mandatory = $true
+            $ParameterAttribute.ParameterSetName = 'ByName'
+            $AttributesCollection.Add($ParameterAttribute)
+            [string[]] $ValidateSet = $script:Users | Select-Object -ExpandProperty AdminName
+            $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($ValidateSet)
+            $AttributesCollection.Add($ValidateSetAttribute)
+            $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string[]], $AttributesCollection)
+            $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+
+            return $RuntimeParameterDictionary
+        #}
+    }# DynamicParam
+    Begin {
+        if ( -not $UserId ) {
+            $UserId = $script:Users | Where-Object {$_.AdminName -eq $($PSBoundParameters.AdminName ) } | Select-Object -ExpandProperty UserId
+        }
+    }# Begin
+    Process {
+    $URISuffix = $URISuffix -f $UserId
+    $URISuffix | Write-Debug
+    
+
+    [hashtable]$Params = @{
+                            'URISuffix' = $URISuffix
+                            'Method'    = 'PUT'
+    }
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+
+    $Params | Out-String | Write-Debug
+
+    return Update-VSAItems @Params
+    }
+}
+Export-ModuleMember -Function Disable-VSAUser

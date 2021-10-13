4a3a8f9a87fc95c0c6be0488026e7b6992bb9829
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2021-10-13 18:28:43 +0200

    Update Set-VSATenantRoletypeLimit.ps1

--- VSA/Agent Procedures/VSA Module/public/Set-VSATenantRoletypeLimit.ps1
+++ VSA/Agent Procedures/VSA Module/public/Set-VSATenantRoletypeLimit.ps1
@@ -10,11 +10,19 @@
     .PARAMETER URISuffix
         Specifies URI suffix if it differs from the default.
     .PARAMETER TenantId
-        Specifies a tenant partition.
-    .PARAMETER Modules
-        Array of modules to be activated.
+        Specifies a tenant partition by Id.
+    .PARAMETER TenantName
+        Specifies a tenant partition by name.
+    .PARAMETER RoleTypeName
+        Specifies a role type by name.
+    .PARAMETER RoleTypeId
+        Specifies a role type by Id.
+    .PARAMETER Limit
+        Number of users.
     .EXAMPLE
-       Set-VSATenantRoletypeLimit -TenantId 10001 -Modules 'Agent', 'Kaseya System Patch'
+       Set-VSATenantRoletypeLimit -Limit 1 -RoleTypeId 2 -TenantId 10001
+    .EXAMPLE
+       Set-VSATenantRoletypeLimit -Limit 1 -RoleTypeName 'KDM Admin' -TenantName 'YourTenantName'
     .INPUTS
        Accepts piped non-persistent VSAConnection 
     .OUTPUTS
@@ -43,14 +51,6 @@
 
         [parameter(Mandatory=$true,
             ValueFromPipelineByPropertyName=$true,
-            ParameterSetName = 'ById')]
-        [parameter(Mandatory=$true,
-            ValueFromPipelineByPropertyName=$true,
-            ParameterSetName = 'ByName')]
-        [decimal] $TenantId,
-
-        [parameter(Mandatory=$true,
-            ValueFromPipelineByPropertyName=$true,
             ParameterSetName = 'ByName')]
         [parameter(Mandatory=$true,
             ValueFromPipelineByPropertyName=$true,
@@ -70,10 +70,10 @@
         [hashtable] $AuxParameters = @{}
         if($VSAConnection) {$AuxParameters.Add('VSAConnection', $VSAConnection)}
 
-        [array] $script:Roles = Get-VSARoleTypes @AuxParameters | Select-Object RoleTypeId, RoleTypeName
-        [array] $script:Tenants = Get-VSATenants @AuxParameters | Select-Object RoleTypeId, RoleTypeName
+        [array] $script:Roles = try {Get-VSARoleTypes @AuxParameters -ErrorAction Stop | Select-Object RoleTypeId, RoleTypeName } catch { Write-Error $_ }
+        [array] $script:Tenants = try {Get-VSATenants @AuxParameters -ErrorAction Stop | Select-Object Id, Ref } catch { Write-Error $_ }
 
-        $ParameterName = 'RoleName' 
+        $ParameterName = 'RoleTypeName' 
         $AttributesCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
         $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
         $ParameterAttribute.Mandatory = $true
@@ -82,7 +82,7 @@
         [string[]] $ValidateSet = $script:Roles | Select-Object -ExpandProperty RoleTypeName
         $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($ValidateSet)
         $AttributesCollection.Add($ValidateSetAttribute)
-        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string[]], $AttributesCollection)
+        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributesCollection)
         $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
 
         $ParameterName = 'RoleTypeId' 
@@ -94,27 +94,64 @@
         [string[]] $ValidateSet = $script:Roles | Select-Object -ExpandProperty RoleTypeId
         $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($ValidateSet)
         $AttributesCollection.Add($ValidateSetAttribute)
-        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string[]], $AttributesCollection)
+        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributesCollection)
+        $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+
+        $ParameterName = 'TenantName' 
+        $AttributesCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+        $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+        $ParameterAttribute.Mandatory = $true
+        $ParameterAttribute.ParameterSetName = 'ByName'
+        $AttributesCollection.Add($ParameterAttribute)
+        [string[]] $ValidateSet = $script:Tenants | Select-Object -ExpandProperty Ref
+        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($ValidateSet)
+        $AttributesCollection.Add($ValidateSetAttribute)
+        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributesCollection)
+        $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+
+        $ParameterName = 'TenantId' 
+        $AttributesCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+        $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+        $ParameterAttribute.Mandatory = $true
+        $ParameterAttribute.ParameterSetName = 'ById'
+        $AttributesCollection.Add($ParameterAttribute)
+        [string[]] $ValidateSet = $script:Tenants | Select-Object -ExpandProperty Id
+        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($ValidateSet)
+        $AttributesCollection.Add($ValidateSetAttribute)
+        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributesCollection)
         $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
 
         return $RuntimeParameterDictionary
     }# DynamicParam
     Begin {
-        if ( -not [string]::IsNullOrEmpty($RoleTypeId) ) {
-            $RoleTypeId = $script:Roles  | Where-Object {$_.RoleName -in $($PSBoundParameters.AdminRoleNames ) } | Select-Object -ExpandProperty RoleId
+        if ( [string]::IsNullOrEmpty($RoleTypeId) ) {
+            $RoleTypeId   = $script:Roles | Where-Object { $_.RoleTypeName -eq $PSBoundParameters.RoleTypeName } | Select-Object -ExpandProperty RoleTypeId
+            $RoleTypeName = $PSBoundParameters.RoleTypeName
         }
-        if ( 0 -eq $AdminScopeIds.Count ) {
-            $AdminScopeIds = $script:Scopes | Where-Object {$_.ScopeName -in $($PSBoundParameters.AdminScopeNames ) } | Select-Object -ExpandProperty ScopeId
+        if ( [string]::IsNullOrEmpty($RoleTypeName) ) {
+            $RoleTypeName   = $script:Roles | Where-Object { $_.RoleTypeId -eq $PSBoundParameters.RoleTypeId } | Select-Object -ExpandProperty RoleTypeName
+            $RoleTypeId = $PSBoundParameters.RoleTypeId
         }
-        if ( -not $DefaultStaffOrgId ) {
-            $DefaultStaffOrgId = $script:Organizations | Where-Object {$_.OrgName -eq $($PSBoundParameters.DefaultStaffOrgName ) } | Select-Object -ExpandProperty OrgId
+        if ( [string]::IsNullOrEmpty($TenantId)  ) {
+            $TenantId = $script:Tenants | Where-Object { $_.Ref -eq $PSBoundParameters.TenantName } | Select-Object -ExpandProperty Id
+            $TenantName = $PSBoundParameters.TenantName
+        }
+        if ( [string]::IsNullOrEmpty($TenantName)  ) {
+            $TenantName = $script:Tenants | Where-Object { $_.Id -eq $PSBoundParameters.TenantId } | Select-Object -ExpandProperty Ref
+            $TenantId = $PSBoundParameters.TenantId
         }
     }# Begin
     Process {
 
         $URISuffix = $URISuffix -f $TenantId
-    
-        $Body = ConvertTo-Json $TenantModues[$Modules]
+        [string] $Body = ConvertTo-Json @(
+                            @{
+                                RoleName = $RoleTypeName
+                                RoleType = [decimal]$RoleTypeId
+                                Limit    = [decimal]$Limit
+                            }
+                        )
+        $Body | Write-Debug
 
         [hashtable]$Params =@{
             URISuffix = $URISuffix

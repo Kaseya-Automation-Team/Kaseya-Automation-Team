217296db0308aceaedbc7926437366703de06d25
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-10-18 13:31:10 +0300

    New function

---
+++ VSA/Agent Procedures/VSA Module/public/Get-VSAStorageContents.ps1
@@ -0,0 +1,77 @@
+function Get-VSAStorageContents {
+    <#
+    .Synopsis
+       Returns remote control recorded session.
+    .DESCRIPTION
+       Returns the file contents of a remote control recording in the body of the response.
+       The {fileId} is specified using the GUID in the AgentFileId column of the Storage.Agent table.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER FileId
+        Specifies AgentFileId from the SQL table.
+    .PARAMETER Filter
+        Specifies REST API Filter.
+    .PARAMETER Paging
+        Specifies REST API Paging.
+    .PARAMETER Sort
+        Specifies REST API Sorting.
+    .EXAMPLE
+       Get-VSAStorageContent -FileId 233434543543543
+    .EXAMPLE
+       Get-VSAStorageContent -VSAConnection $connection -FileId 233434543543543
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       File to download.
+    #>
+
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'NonPersistent')]
+        [VSAConnection] $VSAConnection,
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName = 'Persistent')]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = 'api/v1.0/storage/file/{0}/contents',
+        [parameter(ParameterSetName = 'Persistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [parameter(ParameterSetName = 'NonPersistent', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $FileId,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Filter,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Paging,
+        [Parameter(ParameterSetName = 'Persistent', Mandatory = $false)]
+        [Parameter(ParameterSetName = 'NonPersistent', Mandatory = $false)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Sort
+    )
+
+    $URISuffix = $URISuffix -f $FileId
+
+    [hashtable]$Params =@{
+        URISuffix = $URISuffix
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+    if($Filter)        {$Params.Add('Filter', $Filter)}
+    if($Paging)        {$Params.Add('Paging', $Paging)}
+    if($Sort)          {$Params.Add('Sort', $Sort)}
+
+    return Get-VSAItems @Params
+}
+
+Export-ModuleMember -Function Get-VSAStorageContents

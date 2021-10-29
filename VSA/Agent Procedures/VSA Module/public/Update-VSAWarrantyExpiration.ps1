d185d6f4879e399c491cadb1f94bc6d2ce8adea5
aliaksandr-serzhankou-kaseya <73842930+aliaksandr-serzhankou-kaseya@users.noreply.github.com>
2021-10-29 11:52:22 +0300

    Rename function

---
+++ VSA/Agent Procedures/VSA Module/public/Update-VSAWarrantyExpiration.ps1
@@ -0,0 +1,55 @@
+function Update-VSAWarrantyExpiration
+{
+    <#
+    .Synopsis
+       Updates the purchase date and warranty expiration date for a single agent.
+    .DESCRIPTION
+       Updates the purchase date and warranty expiration date for a single agent.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .EXAMPLE
+       Start-VSAPurchaseWarrantyExpirationDatesUpdate -AgentID 10001
+    .EXAMPLE
+       Start-VSAPurchaseWarrantyExpirationDatesUpdate -AgentID 10001 -VSAConnection $connection
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       True if start of baseline audit was successful.
+    #>
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true)]
+        [VSAConnection] $VSAConnection,
+
+        [parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $URISuffix = 'api/v1.0/assetmgmt/audit/{0}/hardware/purchaseandwarrantyexpire', 
+ 
+        [parameter(Mandatory = $true,  
+            ValueFromPipelineByPropertyName = $true)]
+        [ValidateScript({
+            if( $_ -notmatch "^\d+$" ) {
+                throw "Non-numeric Id"
+            }
+            return $true
+        })]
+        [string] $AgentID
+    )
+    
+    $URISuffix = $URISuffix -f $AgentID
+
+    [hashtable]$Params = @{
+        URISuffix = $($URISuffix -f $AgentID)
+        Method = 'PUT'
+    }
+
+    if($VSAConnection) {$Params.Add('VSAConnection', $VSAConnection)}
+
+    return Update-VSAItems @Params
+}
+Export-ModuleMember -Function Update-VSAWarrantyExpiration

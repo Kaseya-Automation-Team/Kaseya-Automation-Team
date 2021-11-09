35282427dd31ef1c79db11516d0e9f3733e45a48
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2021-11-09 14:05:48 +0100

    Update Update-VSAItems.ps1
    Response processing optimized. Extended output added.

--- VSA/Agent Procedures/VSA Module/private/Update-VSAItems.ps1
+++ VSA/Agent Procedures/VSA Module/private/Update-VSAItems.ps1
@@ -12,6 +12,8 @@
     Specifies URI suffix if it differs from the default.
 .PARAMETER Method
     Specifies REST API Method.
+.PARAMETER ExtendedOutput
+    Specifies wether to return the Result field of the REST API response.
 .EXAMPLE
    Update-VSAItems
 .EXAMPLE
@@ -20,53 +22,41 @@
    Accepts piped non-persistent VSAConnection 
 .OUTPUTS
    True if method call was successful or False elsewhere.
+   The Result field of the REST API response if the ExtendedOutput specified.
 #>
     [CmdletBinding()]
-    [OutputType([bool])]
     param ( 
-        [parameter(Mandatory = $true, 
-            ValueFromPipelineByPropertyName = $true,
-            ParameterSetName = 'NonPersistent')]
+        [parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true)]
         [VSAConnection] $VSAConnection,
 
         [parameter(Mandatory=$true,
-            ValueFromPipelineByPropertyName=$true,
-            ParameterSetName = 'NonPersistent')]
-        [parameter(Mandatory=$true,
-            ValueFromPipelineByPropertyName=$true,
-            ParameterSetName = 'Persistent')]
+            ValueFromPipelineByPropertyName=$true)]
         [ValidateNotNullOrEmpty()] 
         [string] $URISuffix,
 
         [parameter(Mandatory=$true,
-            ValueFromPipelineByPropertyName=$true,
-            ParameterSetName = 'NonPersistent')]
-        [parameter(Mandatory=$true,
-            ValueFromPipelineByPropertyName=$true,
-            ParameterSetName = 'Persistent')]
+            ValueFromPipelineByPropertyName=$true)]
         [ValidateSet("POST", "PUT", "DELETE", "PATCH")]
         [string] $Method,
 
         [parameter(Mandatory=$false,
-            ValueFromPipelineByPropertyName=$true,
-            ParameterSetName = 'NonPersistent')]
-        [parameter(Mandatory=$false,
-            ValueFromPipelineByPropertyName=$false,
-            ParameterSetName = 'Persistent')]
+            ValueFromPipelineByPropertyName=$true)]
         [ValidateNotNullOrEmpty()]
         [string] $Body,
 
         [parameter(Mandatory=$false,
-            ValueFromPipelineByPropertyName=$true,
-            ParameterSetName = 'NonPersistent')]
-        [parameter(Mandatory=$false,
-            ValueFromPipelineByPropertyName=$false,
-            ParameterSetName = 'Persistent')]
+            ValueFromPipelineByPropertyName=$true)]
         [ValidateNotNullOrEmpty()]
-        [string] $ContentType
+        [string] $ContentType,
+
+        [parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true)]
+        [switch] $ExtendedOutput
     )
 
     [bool]$result = $false #by default
+    $ExtendedResult
 
     if ( $null -eq $VSAConnection )
     {
@@ -113,18 +103,24 @@
     $requestParameters | ConvertTo-Json -Depth 3 | Out-String | Write-Debug
 
     #$result = Get-RequestData -URI $CombinedURL -AuthString $UsersToken
-    "Calling Get-RequestData" | Write-Verbose
-    "Calling Get-RequestData" | Write-Debug
+    "$($MyInvocation.MyCommand). Calling Get-RequestData" | Write-Verbose
+    "$($MyInvocation.MyCommand). Calling Get-RequestData" | Write-Debug
     $response = Get-RequestData @requestParameters
-    if ($response) {
-        if ( ($response.ResponseCode -in @(0, 200, 201, 202, 204)) -or ('OK' -eq $response.Status) )
+    if ( $response ) {
+        if ( ($response.ResponseCode -match "(^0$)|(^20\d$)") -or ('OK' -eq $response.Status) )
         {
             $result = $true
+            $ExtendedResult = $response
         }
     } else {
         $result = $true
-        "No response returned" | Write-Debug
-        "No response returned" | Write-Verbose
+        "$($MyInvocation.MyCommand). No response returned" | Write-Debug
+        "$($MyInvocation.MyCommand). No response returned" | Write-Verbose
     }
-    return $result
+
+    if ($ExtendedOutput) {
+        return $ExtendedResult
+    } else {
+        return $result
+    }
 }

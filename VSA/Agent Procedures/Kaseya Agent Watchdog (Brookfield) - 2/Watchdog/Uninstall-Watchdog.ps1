ff7e39d2609afb7b0b7c8b8590410dd86aa3eefd
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2021-12-27 14:26:28 +0100

    Create Uninstall-Watchdog.ps1

---
+++ VSA/Agent Procedures/Kaseya Agent Watchdog (Brookfield) - 2/Watchdog/Uninstall-Watchdog.ps1
@@ -0,0 +1,16 @@
+﻿$isInstalled = Get-ScheduledTask -TaskName "Kaseya Agent Watchdog" -ErrorAction SilentlyContinue
+
+if ($isInstalled) {
+    Write-Host "Kaseya Agent Watchdog task has been found and will be removed."
+
+    try {
+        Unregister-ScheduledTask -TaskName "Kaseya Agent Watchdog" -Confirm:$false -ErrorAction Stop
+    } catch {
+        Write-Host "Unable to delete task:"
+        Write-Host $_.Exception.Message
+    }
+
+    Write-Host "Kaseya Agent Watchdog task has been successfully removed."
+} else {
+    Write-Host "Kaseya Agent task wasn't found on this computer."
+}

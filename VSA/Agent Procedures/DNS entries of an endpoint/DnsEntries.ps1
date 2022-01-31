be60458dde0b7c5c8448033c0b7fe98d92b590b0
SarathMopuri-Kaseya <sarath.mopuri@kaseya.com>
2022-01-31 19:29:45 +0530

    DNS Entries

---
+++ VSA/Agent Procedures/DNS entries of an endpoint/DnsEntries.ps1
@@ -0,0 +1,7 @@
+﻿$ifList = Get-NetAdapter -physical | where status -eq 'Up' | select-object -ExpandProperty 'ifIndex'
+
+ForEach($if in $ifList)
+{
+	Get-DnsClientServerAddress | Where-Object {($_.InterfaceIndex -eq $iflist) -and ($_.ServerAddresses -ne $null)} |Select-Object InterfaceAlias, ServerAddresses | Out-File -FilePath C:\temp\DnsEntries.txt
+}
+

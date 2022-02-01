59ca68f50b878e1079bf59f3b6890983658e0646
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-02-01 13:42:35 +0100

    Update Gather-KAVPwd.ps1

--- Scripts/Powershell/Gather-KAVPwd/Gather-KAVPwd.ps1
+++ Scripts/Powershell/Gather-KAVPwd/Gather-KAVPwd.ps1
@@ -1,10 +1,16 @@
 ﻿param (
+    [Alias("User","UserName")]
     [parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
     [string] $SQLUser,
+    [Alias("Password", "Pwd")]
     [parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
     [string] $SQLPwd,
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+	[string]$OutputFilePath,
+    [Alias("Host","Server")]
     [parameter(Mandatory=$false)]
     [string] $SQLServer = 'localhost',
     [parameter(Mandatory=$false)]
@@ -12,6 +18,8 @@
     [parameter(Mandatory=$false)]
     [switch] $LogIt
 )
+
+$ScriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 
 #region check/start transcript
 [string]$Pref = 'Continue'
@@ -21,12 +29,10 @@ if ( $LogIt )
     $VerbosePreference = $Pref
     $InformationPreference = $Pref
     $ScriptName = [io.path]::GetFileNameWithoutExtension( $($MyInvocation.MyCommand.Name) )
-    $ScriptPath = Split-Path $script:MyInvocation.MyCommand.Path
     $LogFile = "$ScriptPath\$ScriptName.log"
     Start-Transcript -Path $LogFile
 }
 #endregion check/start transcript
-
 
 if([string]::IsNullOrEmpty($SQLServer)) {$SQLServer = 'localhost'}
 "Host\Instance: $SQLServer" | Write-Debug
@@ -44,11 +50,11 @@ else
 }
 
 [string] $ConnectionString = $ConnectionParameters -join ';'
+[string[]] $Fields = @( 'partitionId', 'agentGuid', 'uninstallPassword' )
 
 [scriptblock] $ScriptBlock = {
-
-    [string] $Query = 'USE ksubscribers; SELECT agentGuid, uninstallPassword FROM sec.SecurityAsset WHERE uninstallPassword IS NOT NULL'
-
+    
+    [string] $Query = "USE ksubscribers; SELECT $($using:Fields -join ',') FROM sec.SecurityAsset WHERE uninstallPassword LIKE 'CoveredData2::%'"
     $SqlConnection = New-Object System.Data.SqlClient.SqlConnection  
     $SqlConnection.ConnectionString = $args[0]
 
@@ -82,7 +88,15 @@ else
         }
         if(0 -lt $DataSet.Tables.Count)
         {
-            $DataSet.Tables[0] | Select-Object agentGuid,uninstallPassword | Write-Output
+            [Array] $KAVPwd = $DataSet.Tables[0]
+            $DllPath = Join-Path -Path $using:ScriptPath -ChildPath "Kaseya.AppFoundation.dll"
+            $assembly = [System.Reflection.Assembly]::LoadFrom( $DllPath )
+            foreach($item in $KAVPwd)
+            {
+                $item.uninstallPassword = [Hermes.shared.MaskData]::HashAndUnmaskDataStringForDb($($item.uninstallPassword), $($item.agentGuid))
+                $item.uninstallPassword
+            }
+            $KAVPwd | Write-Output
         }
         $DataSet.Dispose()
         $SqlAdapter.Dispose()
@@ -98,32 +112,10 @@ $InvokeParameters = @{
 ScriptBlock = $ScriptBlock
 Args = @($ConnectionString)
 ConfigurationName = "Microsoft.PowerShell32"
+ComputerName = $SQLServer
 }
 
-if ($UseWindowsAuthentication)
-{
-    'Windows Athentication' | Write-Debug
-    [securestring] $SecurePassword = ConvertTo-SecureString $SQLPwd -AsPlainText -Force
-    [pscredential] $Credential = New-Object System.Management.Automation.PSCredential ($SQLUser, $SecurePassword)
-
-    $InvokeParameters.Add('Credential', $Credential)
-    $InvokeParameters.Add('ComputerName', $env:COMPUTERNAME)
-}
-
-[Array] $KAVPwd = Invoke-Command @InvokeParameters
-
-if(0 -lt $KAVPwd.Count)
-{
-    $assembly = [System.Reflection.Assembly]::LoadFrom("C:\Users\vladislav.semko\OneDrive - Kaseya\Documents\Scripts\Work\PathFinder\Decrypt\Kaseya.AppFoundation.dll")
-    foreach($item in $KAVPwd)
-    {
-        $item.uninstallPassword
-        $item.uninstallPassword = [Hermes.shared.MaskData]::HashAndUnmaskDataStringForDb($($item.uninstallPassword), $($item.agentGuid))
-    }
-}
-
-$KAVPwd  | Write-Output
-
+Invoke-Command @InvokeParameters | Select-Object $Fields | Where-Object {$_.uninstallPassword} | Export-Csv -Path $OutputFilePath -Encoding UTF8 -NoTypeInformation -Force 
 
 #region check/stop transcript
 if ( $LogIt )

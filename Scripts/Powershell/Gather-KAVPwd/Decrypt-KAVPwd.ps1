fa8b5523018fe917c655e32541f1776011b93a3d
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-02-09 16:00:11 +0100

    Create Decrypt-KAVPwd.ps1

---
+++ Scripts/Powershell/Gather-KAVPwd/Decrypt-KAVPwd.ps1
@@ -0,0 +1,25 @@
+﻿param (
+    [parameter(Mandatory=$true)]
+    [ValidateScript({
+            if( $_ -notmatch "^CoveredData2::\w+$" ) {
+                throw "Non-numeric Id"
+            }
+            return $true
+        })]
+    [string] $InputPassword,
+
+    [parameter(Mandatory=$true)]
+    [ValidateScript({
+            if( $_ -notmatch "^\d+$" ) {
+                throw "Non-numeric Id"
+            }
+            return $true
+        })]
+    [string] $AgentGuid
+)
+
+$ScriptPath = Split-Path $script:MyInvocation.MyCommand.Path
+
+$DllPath = Join-Path -Path $ScriptPath -ChildPath "Kaseya.AppFoundation.dll"
+[System.Reflection.Assembly]::LoadFrom( $DllPath ) | Out-Null
+[Hermes.shared.MaskData]::HashAndUnmaskDataStringForDb($InputPassword, $AgentGuid) | Write-Output

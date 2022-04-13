7fff71ce302d429689485d47c6a387a6d6bcd8e0
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-04-13 11:08:13 +0200

    Create Disable-LocalAdmins.ps1

---
+++ VSA/Agent Procedures/CodeRefactoring/Disable MS Office Hardware Acceleration/Disable-LocalAdmins.ps1
@@ -0,0 +1,15 @@
+﻿<#
+    Disables local user accounts that are members of the local Administrators group
+#>
+#Create VSA X Event Source if it doesn't exist
+if ( -not [System.Diagnostics.EventLog]::SourceExists("VSA X")) {
+    [System.Diagnostics.EventLog]::CreateEventSource("VSA X", "Application")
+}
+
+(Get-WMIObject -Class Win32_Group -Filter "LocalAccount=TRUE and SID='S-1-5-32-544'").GetRelated(`
+"Win32_Account", "Win32_GroupUser", "", "", "PartComponent", "GroupComponent", $false, $null) `
+| ForEach-Object {
+    $_.Disabled = $false
+    $_.Put()
+}
+[System.Diagnostics.EventLog]::WriteEntry("VSA X", "Local members of the local Administrators group were disabled", "Information", 200)

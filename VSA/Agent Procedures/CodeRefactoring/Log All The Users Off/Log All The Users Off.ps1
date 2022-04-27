731fd1eb7ebb3add2d58f9f4fcda7decab5ab80b
SarathMopuri-Kaseya <sarath.mopuri@kaseya.com>
2022-04-27 11:46:05 +0530

    Create Log All The Users Off.ps1

---
+++ VSA/Agent Procedures/CodeRefactoring/Log All The Users Off/Log All The Users Off.ps1
@@ -0,0 +1,28 @@
+﻿#This script will logoff all the local users logged on to the machine
+
+$scriptBlock = {
+
+     $ErrorActionPreference = 'Stop'
+
+     try {
+
+         ## Find all sessions that are logged in
+         $sessions = quser | Where-Object { $_ -match '(\d+)\s+Active' }
+
+         ## Parse the session IDs from the output
+         $sessionIds = ($sessions -split ' +')[2]
+         Write-Host "Found $(@($sessionIds).Count) user login(s) on computer."
+
+         ## Loop through each session ID and pass each to the logoff command
+         $sessionIds | ForEach-Object {logoff $_ }
+
+     } catch {
+
+         if ($_.Exception.Message -match 'No user exists') {Write-Host "The user is not logged in."} 
+         
+         else {throw $_.Exception.Message}
+     }
+ }
+
+ ## Run the scriptblock's code on the remote computer
+Invoke-Command -ComputerName localhost -ScriptBlock $scriptBlock

2389ac5897f94033f8b9683e3bf61c99aa79dbc3
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-04-28 18:01:54 +0200

    Update Copy-VSAMachineGroupStructure.ps1

--- VSA/Agent Procedures/VSA Module/public/Copy-VSAMachineGroupStructure.ps1
+++ VSA/Agent Procedures/VSA Module/public/Copy-VSAMachineGroupStructure.ps1
@@ -5,18 +5,16 @@
     .DESCRIPTION
        Creates machine group structure in an organization based on given array of Machine groups.
        Takes either persistent or non-persistent connection information.
-    .PARAMETER VSAConnection
-        Specifies existing non-persistent VSAConnection.
+    .PARAMETER SourceVSA
+        Specifies existing non-persistent connection to the Source Environment.
+    .PARAMETER DestinationVSA
+        Specifies existing non-persistent connection to the Destination Environment.
     .PARAMETER SourceMGs
         Specifies cource array of Machine groups
-    .PARAMETER OrgId
-        Specifies numeric id of organization
-    .PARAMETER ParentMachineGroupId
-        Optional parameter, specifies numeric id of parent machine group
+    .PARAMETER OrgRef
+        Specifies Reference (ID) of organization
     .EXAMPLE
-        Copy-VSAMachineGroupStructure -SourceMGs $SourceMGs -OrgId $DestinationOrgId
-    .EXAMPLE
-        Copy-VSAMachineGroupStructure -SourceMGs $SourceMGs -OrgId $DestinationOrgId -VSAConnection $connection
+        Copy-VSAMachineGroupStructure -SourceMGs $SourceMGs -OrgRef $OrgRef -SourceVSA $SourceVSA -DestinationVSA $DestinationVSA
     .INPUTS
        Accepts piped parameters 
     .OUTPUTS
@@ -26,7 +24,11 @@
     param (
         [parameter(Mandatory = $false, 
             ValueFromPipelineByPropertyName = $true)]
-        [VSAConnection] $VSAConnection,
+        [VSAConnection] $SourceVSA,
+
+        [parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true)]
+        [VSAConnection] $DestinationVSA,
 
         [parameter(Mandatory=$true,
             ValueFromPipelineByPropertyName=$true)]
@@ -35,81 +37,128 @@
         [parameter(Mandatory=$true,
             ValueFromPipelineByPropertyName=$true)]
         [ValidateScript({
-            if( (-not [string]::IsNullOrEmpty($_)) -and ($_ -notmatch "^\d+$") ) {
-                throw "Non-numeric value"
+            if( [string]::IsNullOrEmpty($_) ) {
+                throw "Empty value"
             }
             return $true
         })]
-        [string] $OrgId,
-
-        [parameter(Mandatory=$false,
-            ValueFromPipelineByPropertyName=$true)]
-        [ValidateScript({
-            if( (-not [string]::IsNullOrEmpty($_)) -and ($_ -notmatch "^\d+$") ) {
-                throw "Non-numeric value"
-            }
-            return $true
-        })]
-        [string] $ParentMachineGroupId
+        [string] $OrgRef
     )
-    $SourceMGs = $SourceMGs | Where-Object { -not [string]::IsNullOrEmpty($_.MachineGroupId) }
 
-    [hashtable]$CommonParams = @{ 'OrganizationId' = $OrgId }        
-    if($VSAConnection) {$CommonParams.Add('VSAConnection', $VSAConnection)}
-    $OrgRef = $(Get-VSAOrganization @CommonParams | Select-Object -ExpandProperty OrgRef) -replace '\.', '\.'
+    [hashtable]$SourceParams = @{}
+    if($SourceVSA) {$SourceParams.Add('VSAConnection', $SourceVSA) }
+    $SourceOrg = Get-VSAOrganization @SourceParams | Where-Object {$_.OrgRef -eq $OrgRef}
+    if ( $null -eq $SourceOrg) {
+        throw "Organization $OrgRef not found in the Source VSA"
+    }
+    $SourceOrgId = $SourceOrg | Select-Object -ExpandProperty OrgId
+    $SourceParams.Add('OrgID', $SourceOrgId)
 
-    Foreach ($MachineGroup in $($SourceMGs | Sort-Object -Property MachineGroupName, ParentMachineGroupId) )
+    [array]$global:ExistingSourceMGs = Get-VSAMachineGroup @SourceParams | Where-Object { -not [string]::IsNullOrEmpty($_.MachineGroupId) }
+    $Info = "Source: [$($global:ExistingSourceMGs | Select-Object -ExpandProperty MachineGroupName | Out-String )]"
+    $Info | Write-Host -ForegroundColor Cyan
+    $Info | Write-Debug
+    $Info | Write-Verbose
+
+    [hashtable]$DestinationParams = @{ 'VSAConnection' = $DestinationVSA }
+    $DestinationOrg = Get-VSAOrganization @DestinationParams | Where-Object {$_.OrgRef -eq $OrgRef}
+    if ( $null -eq $SourceOrg) {
+        throw "Organization $OrgRef not found in the Destination VSA"
+    }
+    $DestinationOrgId = $DestinationOrg | Select-Object -ExpandProperty OrgId
+    $DestinationParams.Add('OrgID', $DestinationOrgId)
+
+    [array]$global:DestinationMGs = @()
+    $global:DestinationMGs += Get-VSAMachineGroup @DestinationParams
+
+    Foreach ($MachineGroup in $($SourceMGs | Sort-Object -Property @{Expression = { $_.MachineGroupName.Split('.').Count }}, @{Expression = {$_.ParentMachineGroupId}; Ascending = $false} , @{Expression = {$_.MachineGroupName}; Ascending = $false}) )
     {
-        [hashtable]$CommonParams = @{ 'OrgId' = $OrgId }        
-        if($VSAConnection) {$CommonParams.Add('VSAConnection', $VSAConnection)}
+        [hashtable]$DestinationParams = @{ VSAConnection =$DestinationVSA
+                                           OrgID = $DestinationOrgId
+                                         }
 
-        $DestinationMGs = Get-VSAMachineGroup @CommonParams
+        
         #$SplitName = ($MachineGroup.MachineGroupName | Select-String -Pattern "(?:\.root).*$").Matches.Value
-        $SplitName = ($MachineGroup.MachineGroupName | Select-String -Pattern "(?<=$OrgRef).*$").Matches.Value
+        #$SplitName = ($MachineGroup.MachineGroupName | Select-String -Pattern "(?<=$OrgRef).*$").Matches.Value
+        $global:DestinationMGs = Get-VSAMachineGroup @DestinationParams
+        [array]$CheckDestination = $global:DestinationMGs | Where-Object { $_.MachineGroupName -eq $MachineGroup.MachineGroupName }
 
-        [array]$CheckDestination = $DestinationMGs | Where-Object {$_.MachineGroupName -match "$SplitName`$"}
-
-        $Info = "CheckDestination <$SplitName>  $($CheckDestination.Count)"
+        $Info = "CheckDestination <$($MachineGroup.MachineGroupName)>  $($CheckDestination.Count)"
+        $Info | Write-Host
         $Info | Write-Debug
         $Info | Write-Verbose
 
-        $NameToCreate = ($SplitName.split('.'))[-1]
+        $NewMGObj = $MachineGroup
+        
+        #region Check if there's Parent Machine Group and the Parent Machine Group already exists in the destination.
+        if( -not [string]::IsNullOrEmpty( $MachineGroup.ParentMachineGroupId ) ) {
+            $Info = "Look for parent of <$($MachineGroup.MachineGroupName)> in [$($global:ExistingSourceMGs | Select-Object -ExpandProperty MachineGroupName | Out-String )]"
+            $Info | Write-Host -ForegroundColor Yellow
+            $Info | Write-Verbose
+            $Info | Write-Debug
+            #obtain ParentMachineGroupId by MachineGroupName
+            $ParentMachineGroupName = $global:ExistingSourceMGs | Where-Object { $_.MachineGroupId -eq $MachineGroup.ParentMachineGroupId } | Select-Object -ExpandProperty MachineGroupName
+            if ( [string]::IsNullOrEmpty($ParentMachineGroupName) ) {
+                $Info = "Parent Organazation for <$($MachineGroup.MachineGroupName)> not found in the source. Skip now"
+                $Info | Write-Host -ForegroundColor DarkRed -BackgroundColor White
+                $Info | Write-Verbose
+                $Info | Write-Debug
+                continue
+            } else {
+                $ParentMachineGroupId = $global:DestinationMGs | Where-Object { $_.MachineGroupName -eq $ParentMachineGroupName } | Select-Object -ExpandProperty MachineGroupId
+
+                $Info = "Parent Organazation for <$($MachineGroup.MachineGroupName)> : <$ParentMachineGroupName>. Destination ParentMachineGroupId <$ParentMachineGroupId>. Can be created"
+                $Info | Write-Host -ForegroundColor DarkGreen -BackgroundColor White
+                $Info | Write-Verbose
+                $Info | Write-Debug
+
+                $NewMGObj.ParentMachineGroupId = $ParentMachineGroupId 
+            }
+        }
+        #endregion Check if there's Parent Machine Group and the Parent Machine Group already exists in the destination.
     
         if( 0 -eq $CheckDestination.Count) #No MG with this name in the destination
         {
-            $AddMGParams = $CommonParams.Clone()
+            $AddMGParams = $DestinationParams.Clone()
+            $AddMGParams.Add('ExtendedOutput', $true)
 
-            $AddMGParams.Add('ExtendedOutput',       $true)
-            $AddMGParams.Add('MachineGroupName',     $NameToCreate)
-            $AddMGParams.Add('ParentMachineGroupId', $ParentMachineGroupId)
+            #New MG Params
+            $NameToCreate = ($($MachineGroup.MachineGroupName).split('.'))[-1]
+            $NewMGObj.MachineGroupName = $NameToCreate
 
-            $AddMGParams | Out-String | Write-Debug
-            #Create a new MG
-            $GroupId = Add-VSAMachineGroup @AddMGParams
-        }
-        else # An MG with this name already exists in the destination
-        { 
+            
+
+            $GroupId = $NewMGObj | Add-VSAMachineGroup @AddMGParams
+            $global:DestinationMGs = Get-VSAMachineGroup @DestinationParams
+        } else  { # A MG with this name already exists in the destination
             $GroupId  = $CheckDestination.MachineGroupId
         }
 
         $Info = "GroupId: $GroupId"
+        $Info | Write-Host
         $Info | Write-Debug
         $Info | Write-Verbose
 
-        [array]$DirectChildren = $SourceMGs | Where-Object {$_.ParentMachineGroupId -eq $MachineGroup.MachineGroupId }
+        [array]$DirectChildren = $global:ExistingSourceMGs | Where-Object {$_.ParentMachineGroupId -eq $MachineGroup.MachineGroupId }
 
-        $Info = "DirectChildren for <$NameToCreate> $($DirectChildren.Count)"
+        $Info = "Count Direct Children <$($MachineGroup.MachineGroupName)> $($DirectChildren.Count)"
+        $Info | Write-Host
         $Info | Write-Debug
         $Info | Write-Verbose
         $DirectChildren | Select-Object -ExpandProperty MachineGroupName | Out-String | Write-Debug
 
         if ( 0 -lt $DirectChildren.Count)
         {
+            $Info = "Direct children of <$($MachineGroup.MachineGroupName)> : [$(($DirectChildren | Select-Object -ExpandProperty MachineGroupName) -join '; ')]"
+            $Info | Write-Host
+            $Info | Write-Debug
+            $Info | Write-Verbose
 
-            [hashtable]$CreateMGParams = $CommonParams.Clone()
-
-            $CreateMGParams.Add('SourceMGs',            $DirectChildren)
-            $CreateMGParams.Add('ParentMachineGroupId', $GroupId)
+            [hashtable]$CreateMGParams = @{ OrgRef = $OrgRef
+                                            SourceVSA = $SourceVSA
+                                            DestinationVSA = $DestinationVSA
+                                            SourceMGs = $DirectChildren
+                                            }
 
             $CreateMGParams | Out-String | Write-Debug
 
@@ -118,4 +167,4 @@
 
     } # Foreach ($MachineGroup in $SourceMGs)
 }
-Export-ModuleMember -Function Copy-VSAMachineGroupStructure
+Export-ModuleMember  -Function Copy-VSAMachineGroupStructure

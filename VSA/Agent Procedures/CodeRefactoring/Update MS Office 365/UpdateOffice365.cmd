2c06a77e1949aec3b4413a6f6d9e9133b3d434b4
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-05-17 14:06:29 +0200

    Create UpdateOffice365.cmd

---
+++ VSA/Agent Procedures/CodeRefactoring/Update MS Office 365/UpdateOffice365.cmd
@@ -0,0 +1,10 @@
+:: Switches the Office 365 update to the monthly update channel and triggers update. All the Office 365 applications will be forced to shutdown.
+@echo off
+SET C2RC="%PROGRAMFILES%\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe"
+if exist "%C2RC%" (
+    "%C2RC%" /changesetting Channel=Current
+    "%C2RC%" /update user updatepromptuser=false forceappshutdown=true displaylevel=false
+    eventcreate /L Application /T INFORMATION /SO VSAX /ID 200 /D "Office update channel was switched them to the monthly update. Update was triggered" > nul
+) else (
+    eventcreate /L Application /T ERROR /SO VSAX /ID 400 /D "Could not locate the %C2RC% file" > nul
+)

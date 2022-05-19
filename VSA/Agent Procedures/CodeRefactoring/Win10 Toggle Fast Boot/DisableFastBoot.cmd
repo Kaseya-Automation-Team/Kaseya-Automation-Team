4702a2e4c258c26208a3f33c09ead95b7dc21f0a
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-05-19 13:48:00 +0200

    Update DisableFastBoot.cmd

--- VSA/Agent Procedures/CodeRefactoring/Win10 Toggle Fast Boot/DisableFastBoot.cmd
+++ VSA/Agent Procedures/CodeRefactoring/Win10 Toggle Fast Boot/DisableFastBoot.cmd
@@ -4,7 +4,7 @@ setlocal
 for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
 if "%version%" == "10.0" (
     powercfg -h off
-    eventcreate /L Application /T INFORMATION /SO VSAX /ID 200 /D "Windows Fast Boot enabled" > nul
+    eventcreate /L Application /T INFORMATION /SO VSAX /ID 200 /D "Windows Fast Boot disabled" > nul
 ) else (
     eventcreate /L Application /T ERROR /SO VSAX /ID 400 /D "Designed for Windows 10 & above" > nul
 )

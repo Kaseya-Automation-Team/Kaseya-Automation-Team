cd8ca8203c00411b51220eedd0a8e04032696b14
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-05-25 15:57:22 +0200

    Create Set-PasswordExpires.ps1

---
+++ VSA/Agent Procedures/CodeRefactoring/Set Password Expiration/Set-PasswordExpires.ps1
@@ -0,0 +1,6 @@
+﻿#Create VSA X Event Source if it doesn't exist
+if ( -not [System.Diagnostics.EventLog]::SourceExists("VSA X")) {
+    [System.Diagnostics.EventLog]::CreateEventSource("VSA X", "Application")
+}
+Get-WmiObject -Class Win32_UserAccount -Filter "LocalAccount=True AND Disabled=False AND PasswordExpires=False" -Property PasswordExpires | ForEach-Object {$_.PasswordExpires=$true; $_.Put()}
+[System.Diagnostics.EventLog]::WriteEntry("VSA X", 'The "Password never expires" property disabled for all local users', "Information", 200)

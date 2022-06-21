b15e3ca8e01965826cb423913feea0822bbc0965
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-06-21 14:47:13 +0200

    Update Clear-CachedDomainCredentials.ps1

--- VSA/Agent Procedures/Procedure [Win] [Domain] Clear Cached Domain Credentials/Clear-CachedDomainCredentials.ps1
+++ VSA/Agent Procedures/Procedure [Win] [Domain] Clear Cached Domain Credentials/Clear-CachedDomainCredentials.ps1
@@ -1,39 +1,24 @@
-﻿#region Get Cached Domain credentials
-Write-Debug 'Collecting cached credentials'
-[string[]]$RawOutput = cmdkey /list
+﻿#Get cached credentials
+[string[]]$CmdkeyOutput = cmdkey /list | ForEach-Object { $_.Trim() | Write-Output } | Where-Object { -not [string]::IsNullOrEmpty( $_ ) }
 
-$Result = @()
-#structurize output data
-foreach ( $Entry in $RawOutput )
-{
-    if ( -not [string]::IsNullOrEmpty($Entry) ) {
-        $Entry = $Entry.Trim()
-
-        if ($Entry.Contains('Target: ')) {
-            $Target = $Entry.Replace('Target: ', '')
-        }
-        if ($Entry.Contains('Type: ')) {
-            $TargetType = $Entry.Replace('Type: ', '')
-        }
-        if ($Entry.Contains('User: ')) {
-            $User = $Entry.Replace('User: ', '')
-
-            $Result += [PSCustomObject]@{
-                Target = $Target
-                Type   = $TargetType
-                User   = $User}
-        }
-    } # ( -not [string]::IsNullOrEmpty($Entry) )
+$CachedCreds = @()
+#Structurize raw cmdkey output 
+foreach ( $Entry in $CmdkeyOutput ) {
+    if ($Entry.Contains('Target: ')) {
+        $Target = $Entry.Replace('Target: ', '')
+    }
+    if ($Entry.Contains('Type: ')) {
+        $TargetType = $Entry.Replace('Type: ', '')
+        $CachedCreds += [PSCustomObject]@{
+            Target = $Target
+            Type   = $TargetType}
+    }
 }
 
 #Filter Cached Domain credentials
-$Result = $Result | Where-Object {$_.Type -match "^Domain"}
-#endregion Get Cached Domain credentials
+$CachedCreds = $CachedCreds | Where-Object {$_.Type -match '^Domain'}
 
-#region Clear Cached Domain credentials
-#Write-Debug 'Deleting cached credentials'
-foreach ( $Entry in $Result ) {
-    #Write-Debug $($Entry.User)
+#Clear Cached Domain credentials
+foreach ( $Entry in $CachedCreds ) {
     cmdkey /delete:$($Entry.Target)
 }
-#endregion Clear Cached Domain credentials

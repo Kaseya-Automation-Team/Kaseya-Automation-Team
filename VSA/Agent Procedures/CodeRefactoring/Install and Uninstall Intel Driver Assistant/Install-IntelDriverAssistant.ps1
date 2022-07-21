4bcb390b5094dcb9a234184f0230d38293a8d436
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-07-21 16:21:54 +0200

    Update Install-IntelDriverAssistant.ps1
    The variable $source renamed to $URL.
    $URL and $Destination variables moved to parameters
    Write-Host replaced Write-Output so that the output can be read by the VSA agent procedure.

--- VSA/Agent Procedures/CodeRefactoring/Install and Uninstall Intel Driver Assistant/Install-IntelDriverAssistant.ps1
+++ VSA/Agent Procedures/CodeRefactoring/Install and Uninstall Intel Driver Assistant/Install-IntelDriverAssistant.ps1
@@ -1,15 +1,15 @@
-﻿Function Get-InstallStatus {
+﻿param (
+    [parameter(Mandatory=$false)]
+    [string] $URL ="https://dsadata.intel.com/installer",
+    [parameter(Mandatory=$false)]
+    [string] $Destination = "$env:TEMP\Intel-Driver-and-Support-Assistant-Installer.exe"
+)
+
+Function Get-InstallStatus {
 
     Return  (Get-Package | Where-Object {$_.Name -eq "Intel® Driver & Support Assistant"} |  Select-Object -Property Status).Status
 
 }
-
-
-# Source file location
-$source = "https://dsadata.intel.com/installer"
-
-# Destination to save the file
-$destination = "$env:TEMP\Intel-Driver-and-Support-Assistant-Installer.exe"
 
 
 $status = Get-InstallStatus
@@ -17,10 +17,10 @@ $status = Get-InstallStatus
 
 if ($Status -ne "Installed"){
     
-    Write-Host "Intel Driver Assistant is not installed on this computer and proceeding with the insallation steps!"
+    Write-Output "Intel Driver Assistant is not installed on this computer and proceeding with the insallation steps!"
 
     #Download the file
-    Invoke-WebRequest -Uri $source -OutFile $destination
+    Invoke-WebRequest -Uri $URL -OutFile $destination
 
     #Run the install command
     Start-Process -Wait -FilePath $destination -ArgumentList '/install /quiet /norestart' 
@@ -32,15 +32,15 @@ if ($Status -ne "Installed"){
     $status = Get-InstallStatus
     if ($Status -eq "Installed"){
     
-        Write-Host "Intel Driver Assistant is successfully installed on this computer!"
+        Write-Output "Intel Driver Assistant is successfully installed on this computer!"
     }
     else{
-      Write-Host "Intel Driver Assistant is not installed!"
+      Write-Output "Intel Driver Assistant is not installed!"
     }
 
 }
 else {
 
-    Write-Host "Intel Driver Assistant is already installed on this computer!"
+    Write-Output "Intel Driver Assistant is already installed on this computer!"
 
 }

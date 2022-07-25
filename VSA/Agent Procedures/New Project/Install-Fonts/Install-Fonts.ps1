55e786fbc738b19cfa3eb72d7baf28671923f9f0
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-07-25 11:43:34 +0200

    Update Install-Fonts.ps1

--- VSA/Agent Procedures/New Project/Install-Fonts/Install-Fonts.ps1
+++ VSA/Agent Procedures/New Project/Install-Fonts/Install-Fonts.ps1
@@ -4,24 +4,20 @@ $objShell = New-Object -ComObject Shell.Application
 $objFolder = $objShell.Namespace($FONTS)
 $Fontdir = dir $Path
 foreach($File in $Fontdir) {
-if(!($file.name -match "pfb$"))
-{
-$try = $true
-$installedFonts = @(Get-ChildItem c:\windows\fonts | Where-Object {$_.PSIsContainer -eq $false} | Select-Object basename)
-$name = $File.baseName
+    if( -not ($file.name -match "pfb$") ) {
+        $try = $true
+        $installedFonts = @(Get-ChildItem "$env:SystemRoot\fonts" | Where-Object {$_.PSIsContainer -eq $false} | Select-Object basename)
+        $name = $File.baseName
 
-foreach($font in $installedFonts)
-{
-$font = $font -replace "_", "" 
-$name = $name -replace "_", "" 
-if($font -match $name)
-{
-$try = $false
-}
-}
-if($try)
-{
-$objFolder.CopyHere($File.fullname)
-}
-}
+        foreach($font in $installedFonts) {
+            $font = $font -replace "_", "" 
+            $name = $name -replace "_", "" 
+            if($font -match $name) {
+                $try = $false
+            }
+        }
+        if($try) {
+            $objFolder.CopyHere($File.fullname)
+        }
+    }
 }

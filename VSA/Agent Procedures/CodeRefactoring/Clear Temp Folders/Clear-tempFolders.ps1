a445f443670e10bfed6f90498f9e0cb557a682c0
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-07-28 12:53:32 +0200

    Update Clear-tempFolders.ps1

--- VSA/Agent Procedures/CodeRefactoring/Clear Temp Folders/Clear-tempFolders.ps1
+++ VSA/Agent Procedures/CodeRefactoring/Clear Temp Folders/Clear-tempFolders.ps1
@@ -5,28 +5,26 @@ Get-ItemProperty -Path Registry::'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Co
 #Clear users' temp folders
 [string] $SIDPattern = 'S-1-5-21-(\d+-?){4}$'
 Get-WmiObject Win32_UserProfile | Where-Object {$_.SID -match $SIDPattern} | Select-Object LocalPath, SID | `
-    ForEach-Object {
-        $UserProfilePath = $_.LocalPath
+ForEach-Object {
+    $UserProfilePath = $_.LocalPath
 
-        [bool] $IsProfileLoaded = Test-Path Registry::HKEY_USERS\$($_.SID)
-        If ( -Not $IsProfileLoaded )
-        {
-            reg load "HKU\$($_.SID)" "$UserProfilePath\ntuser.dat"
-        }
-
-        [string] $TempFolderPath = Get-ItemProperty -Path Registry::$(Join-Path -Path "HKEY_USERS\$($_.SID)" -ChildPath "Environment") -Name "TEMP" | Select-Object -ExpandProperty "TEMP"
-        <#
-        Typically, the path to the user's TEMP folder in the registry contains a relative path that refers to the USERPROFILE system variable.
-        When the registry value is read, the runtime automatically places the running process owner's profile path in the USERPROFILE variable.
-        Therefore, to get the correct path to the user's TEMP folder, the registry value referencing USERPROFILE must be corrected by replacing the process owner's profile path with the user's profile path.
-        #>
-        $RunningProcessProfilePath = $env:USERPROFILE
-
-        $TempFolderPath.Replace($RunningProcessProfilePath, $UserProfilePath) | Clear-Folder
-
-        [gc]::Collect()
-        If ( -Not $IsProfileLoaded )
-        {
-            reg unload "HKU\$($_.SID)"
-        }
+    [bool] $IsProfileLoaded = Test-Path Registry::HKEY_USERS\$($_.SID)
+    if ( -Not $IsProfileLoaded ) {
+        reg load "HKU\$($_.SID)" "$UserProfilePath\ntuser.dat"
     }
+
+    [string] $TempFolderPath = Get-ItemProperty -Path Registry::$(Join-Path -Path "HKEY_USERS\$($_.SID)" -ChildPath "Environment") -Name "TEMP" | Select-Object -ExpandProperty "TEMP"
+    <#
+    Typically, the path to the user's TEMP folder in the registry contains a relative path that refers to the USERPROFILE system variable.
+    When the registry value is read, the runtime automatically places the running process owner's profile path in the USERPROFILE variable.
+    Therefore, to get the correct path to the user's TEMP folder, the registry value referencing USERPROFILE must be corrected by replacing the process owner's profile path with the user's profile path.
+    #>
+    $RunningProcessProfilePath = $env:USERPROFILE
+
+    $TempFolderPath.Replace($RunningProcessProfilePath, $UserProfilePath) | Clear-Folder
+
+    [gc]::Collect()
+    if ( -Not $IsProfileLoaded ) {
+        reg unload "HKU\$($_.SID)"
+    }
+}

14c0ef1929c63c6160b22a7fe1f642c12edacc7e
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-07-29 14:19:31 +0200

    Create Gather-MSTeamsInfo.ps1

---
+++ VSA/Agent Procedures/CodeRefactoring/Gather MS Teams Info/Gather-MSTeamsInfo.ps1
@@ -0,0 +1,33 @@
+﻿#Create VSA Event Source if it doesn't exist
+if ( -not [System.Diagnostics.EventLog]::SourceExists("VSA X")) {
+    [System.Diagnostics.EventLog]::CreateEventSource("VSA X", "Application")
+}
+
+[string] $SIDPattern = 'S-1-5-21-\d+-\d+\-\d+\-\d+$'
+[string] $RegKeyUserProfiles = 'HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*'
+
+[array] $ProfileList = Get-ItemProperty -Path Registry::$RegKeyUserProfiles | `
+            Select-Object  @{name='User';expression={$_.PSChildName}}, 
+            ProfileImagePath `
+                | Where-Object {$_.User -match $SIDPattern}
+
+[array] $TeamsInfo = @()
+
+Foreach ( $Profile in $ProfileList )
+{
+    #Try to resolve user name. Return SID if the name can not be resolved
+    $Account = New-Object Security.Principal.SecurityIdentifier($Profile.User)
+    $UserBySID  = $(  try { $Account.Translate([Security.Principal.NTAccount]) `
+            | Select-Object -ExpandProperty Value
+            } catch {
+                $_.Exception.Message
+            } )
+    if ( $UserBySID -notmatch 'Exception' ) {
+        $Profile.User = $UserBySID
+    }
+    $TeamsPath = Join-Path -Path $Profile.ProfileImagePath -ChildPath "AppData\Roaming\Microsoft\Teams\settings.json"
+    if (Test-Path -Path $TeamsPath) {
+        $TeamsInfo += Get-Content -Path $TeamsPath | ConvertFrom-Json | Select-Object @{name='User';expression={$Profile.User}}, Version, Ring, Environment
+    }
+}
+$TeamsInfo | Out-String | Write-Output

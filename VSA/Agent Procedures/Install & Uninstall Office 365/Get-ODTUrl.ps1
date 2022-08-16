fd9a5cbf372f89cef5b37e8204a8f258d5b62382
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-08-16 09:57:28 +0200

    Create Get-ODTUrl.ps1

---
+++ VSA/Agent Procedures/Install & Uninstall Office 365/Get-ODTUrl.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.Synopsis
+   Gets the URL of latest version of the ODT.
+.NOTES
+   Version 0.1
+   Author: Proserv Team - VS
+.EXAMPLE
+   .\Get-ODTUrl.ps1
+#>
+
+param (
+    [parameter(Mandatory=$false)]
+    [string] $PageUri = 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117'
+)
+
+try {
+    Set-Variable ProgressPreference SilentlyContinue
+    $WebResponse = Invoke-WebRequest -Uri $PageUri -UseBasicParsing -ErrorAction Stop
+} catch {
+    "$PageUri response: [$($_.Exception.Response.StatusCode.Value__)]" | Write-Debug
+}
+
+if ($null -ne $WebResponse) { #URL responded
+    #Look for actual download URL
+    [string]$DownloadUri = $WebResponse.Links | Where-Object outerHTML -Match 'click here to download manually' | Select-Object -ExpandProperty href -Unique
+    if ( -Not [string]::IsNullOrEmpty($DownloadUri)) {
+        $DownloadUri | Write-Output
+    } else {
+        "Failed" | Write-Output
+    }
+}

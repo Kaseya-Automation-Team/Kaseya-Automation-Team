9bfb7978189cad2948550d7f62cc42535ffd3053
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-08-18 12:43:42 +0200

    Update Get-ODTUrl.ps1

--- VSA/Agent Procedures/Install & Uninstall Office 2019/Get-ODTUrl.ps1
+++ VSA/Agent Procedures/Install & Uninstall Office 2019/Get-ODTUrl.ps1
@@ -13,25 +13,20 @@ param (
     [string] $PageUri = 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117'
 )
 
-try
-{
+try {
+    Set-Variable ProgressPreference SilentlyContinue
     $WebResponse = Invoke-WebRequest -Uri $PageUri -UseBasicParsing -ErrorAction Stop
-}
-catch
-{
+} catch {
     "$PageUri response: [$($_.Exception.Response.StatusCode.Value__)]" | Write-Debug
 }
 
-if ($null -ne $WebResponse) #URL responded
-{
+if ($null -ne $WebResponse) { #URL responded
     #Look for actual download URL
     [string]$DownloadUri = $WebResponse.Links | Where-Object outerHTML -Match 'click here to download manually' | Select-Object -ExpandProperty href -Unique
-    if ( -Not [string]::IsNullOrEmpty($DownloadUri))
-    {
-        $DownloadUri | Write-Output
+    if ( [string]::IsNullOrEmpty($DownloadUri)) {
+        $DownloadUri = 'Failed' 
     }
-    else
-    {
-        "Failed" | Write-Output
-    }
+} else {
+    $DownloadUri = 'Failed' 
 }
+$DownloadUri | Write-Output

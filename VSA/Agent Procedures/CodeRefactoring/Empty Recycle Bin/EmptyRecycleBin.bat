14b293a4fddf85d132cd0dfb79c1d9c31e1d43ec
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 17:07:41 +0200

    Update EmptyRecycleBin.bat

--- VSA/Agent Procedures/CodeRefactoring/Empty Recycle Bin/EmptyRecycleBin.bat
+++ VSA/Agent Procedures/CodeRefactoring/Empty Recycle Bin/EmptyRecycleBin.bat
@@ -1,1 +1,16 @@
+::=================================================================================
+::Script Name:        Management: Empty Recycle Bin
+::Description:        Empty Recycle Bin for users.
+::Lastest version:    2022-04-21
+::=================================================================================
+::
+::
+::
+::Required variable inputs:
+::None
+::
+::
+::
+::Required variable outputs:
+::None
 rd /s /q %systemdrive%\$Recycle.Bin

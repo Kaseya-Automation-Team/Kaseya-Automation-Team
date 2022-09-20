e05d418ced1f049eed9155b2d20f3fe74086dc2c
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 21:01:31 +0200

    Update Gather-EncryptionStatus.ps1

--- VSA/Agent Procedures/CodeRefactoring/Gather Encryption Status/Gather-EncryptionStatus.ps1
+++ VSA/Agent Procedures/CodeRefactoring/Gather Encryption Status/Gather-EncryptionStatus.ps1
@@ -1,3 +1,20 @@
+<#
+=================================================================================
+Script Name:        Audit: Security: Gather Encryption Status.
+Description:        Gather BitLocker Encryption Status.
+Lastest version:    2022-06-09
+=================================================================================
+
+
+
+Required variable inputs:
+None
+
+
+
+Required variable outputs:
+None
+#>
 #This script checks status of BitLocker encryption
 
 # Outputs

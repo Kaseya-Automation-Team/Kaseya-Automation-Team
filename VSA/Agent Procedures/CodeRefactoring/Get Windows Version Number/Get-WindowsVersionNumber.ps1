6189b6dbdb663993520d3498b57c33953fc5040f
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 18:05:19 +0200

    Create Get-WindowsVersionNumber.ps1

---
+++ VSA/Agent Procedures/CodeRefactoring/Get Windows Version Number/Get-WindowsVersionNumber.ps1
@@ -0,0 +1,23 @@
+﻿<#
+=================================================================================
+Script Name:        Audit: Get Windows Version Number.
+Description:        Retrieves the build number (10763) and converts it to 21H1.
+Lastest version:    2022-09-20
+=================================================================================
+
+
+
+Required variable inputs:
+None
+
+
+
+Required variable outputs:
+WindowsBuildNumber
+#>
+# Outputs
+$WindowsBuildNumber = "Not Scanned"
+$WindowsBuildNumber = (Get-Item 'HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion').GetValue('ReleaseID')
+
+Start-Process -FilePath "$env:PWY_HOME\CLI.exe" -ArgumentList ("setVariable WindowsBuildNumber ""$WindowsBuildNumber""") -Wait
+

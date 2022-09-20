755ee2102e25ecdb390ad21010e1825aff084b2f
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 19:54:51 +0200

    Update Install-Teams.ps1

--- VSA/Agent Procedures/CodeRefactoring/Install-Uninstall MS Teams/Install-Teams.ps1
+++ VSA/Agent Procedures/CodeRefactoring/Install-Uninstall MS Teams/Install-Teams.ps1
@@ -1,4 +1,20 @@
-## This script downloads and silently installs latest version of Microsoft Teams from the official website
+<#
+=================================================================================
+Script Name:        Software Management: Install Microsoft Teams.
+Description:        Downloads and silently installs Microsoft Teams.
+Lastest version:    2022-06-01
+=================================================================================
+
+
+
+Required variable inputs:
+None
+
+
+
+Required variable outputs:
+None
+#>
 
 #Define variables
 $AppName = "Microsoft Teams"

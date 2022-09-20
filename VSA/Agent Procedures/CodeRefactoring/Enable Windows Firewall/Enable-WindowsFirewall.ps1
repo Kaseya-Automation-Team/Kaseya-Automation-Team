c78e4bed16272d86d0a627cf4f75bea2d5583eeb
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 17:40:35 +0200

    Create Enable-WindowsFirewall.ps1

---
+++ VSA/Agent Procedures/CodeRefactoring/Enable Windows Firewall/Enable-WindowsFirewall.ps1
@@ -0,0 +1,21 @@
+﻿<#
+=================================================================================
+Script Name:        Management: Enable Windows Firewall.
+Description:        This script silently enables firewall on Windows machines.
+Lastest version:    2022-09-20
+=================================================================================
+
+
+
+Required variable inputs:
+None
+
+
+
+Required variable outputs:
+None
+#>
+#Enable Windows Firewall
+Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled True
+Write-Host "Windows Firewall has been enabled"
+eventcreate /L Application /T INFORMATION /SO VSAX /ID 200 /D "Windows Firewall has been enabled" | Out-Null

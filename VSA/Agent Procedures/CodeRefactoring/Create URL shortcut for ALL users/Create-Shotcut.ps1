261e6f763c39a7071984a369f108c4f9d0b38730
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 16:19:59 +0200

    Update Create-Shotcut.ps1
    Description added. Error fixed

--- VSA/Agent Procedures/CodeRefactoring/Create URL shortcut for ALL users/Create-Shotcut.ps1
+++ VSA/Agent Procedures/CodeRefactoring/Create URL shortcut for ALL users/Create-Shotcut.ps1
@@ -1,7 +1,24 @@
+<#
+=================================================================================
+Script Name:        Management: Create URL shortcut for ALL users
+Description:        This script creates URL shortcut on desktop for ALL users. Customize the script by editing the ShortcutURL variable
+Lastest version:    2022-09-20
+=================================================================================
+
+
+
+Required variable inputs:
+None
+
+
+
+Required variable outputs:
+None
+#>
 # Outputs
 $ShortcutName = "Google"
 $ShortcutURL = "https://google.com"
-$Path = "$env:SystemDrive\Users\Public\Desktop\$ShortcutName.url"
+$Path = "%PUBLIC%\Desktop\$ShortcutName.url"
 
 $Source = @"
 [InternetShortcut]

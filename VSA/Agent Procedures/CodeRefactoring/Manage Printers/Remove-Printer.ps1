250faf60e45cabb0c914e55691621cc190bd93ab
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 20:03:14 +0200

    Update Remove-Printer.ps1

--- VSA/Agent Procedures/CodeRefactoring/Manage Printers/Remove-Printer.ps1
+++ VSA/Agent Procedures/CodeRefactoring/Manage Printers/Remove-Printer.ps1
@@ -1,2 +1,19 @@
+<#
+=================================================================================
+Script Name:        Management: Remove printer.
+Description:        Remove printer.
+Lastest version:    2022-07-29
+=================================================================================
+
+
+
+Required variable inputs:
+None
+
+
+
+Required variable outputs:
+None
+#>
 $PrinterName = ''
 Remove-Printer -Name $PrinterName -ErrorAction Continue

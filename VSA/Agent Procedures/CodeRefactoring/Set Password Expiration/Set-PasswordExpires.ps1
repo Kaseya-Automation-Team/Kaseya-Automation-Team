581b3f243b69d74a94031452a9ef80da57bcda92
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 20:37:34 +0200

    Update Set-PasswordExpires.ps1

--- VSA/Agent Procedures/CodeRefactoring/Set Password Expiration/Set-PasswordExpires.ps1
+++ VSA/Agent Procedures/CodeRefactoring/Set Password Expiration/Set-PasswordExpires.ps1
@@ -1,4 +1,21 @@
-﻿#Create VSA X Event Source if it doesn't exist
+﻿<#
+=================================================================================
+Script Name:        Management: Set Password Expiration.
+Description:        Set Password Expiration.
+Lastest version:    2022-05-25
+=================================================================================
+
+
+
+Required variable inputs:
+None
+
+
+
+Required variable outputs:
+None
+#>
+#Create VSA X Event Source if it doesn't exist
 if ( -not [System.Diagnostics.EventLog]::SourceExists("VSA X")) {
     [System.Diagnostics.EventLog]::CreateEventSource("VSA X", "Application")
 }

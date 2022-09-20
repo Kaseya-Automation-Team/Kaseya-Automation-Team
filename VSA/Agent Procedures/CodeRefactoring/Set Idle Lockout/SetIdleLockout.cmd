afead7212a01e19f992ae5ff1243624471f10caa
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 12:27:46 +0200

    Update SetIdleLockout.cmd

--- VSA/Agent Procedures/CodeRefactoring/Set Idle Lockout/SetIdleLockout.cmd
+++ VSA/Agent Procedures/CodeRefactoring/Set Idle Lockout/SetIdleLockout.cmd
@@ -1,3 +1,20 @@
+::
+::=================================================================================
+::Script Name:        Management: Set Idle Lockout to 15min
+::Description:        The script sets Screen Idle Lock timeout to 15 minutes for system on power / on battery
+::Lastest version:    2022-04-11
+::=================================================================================
+::
+::
+::
+::Required variable inputs:
+::None
+::
+::
+::
+::Required variable outputs:
+::None
+::
 ::set idle delay to 15 min
 SET IDLE_MIN=15
 ::convert idle delay to seconds

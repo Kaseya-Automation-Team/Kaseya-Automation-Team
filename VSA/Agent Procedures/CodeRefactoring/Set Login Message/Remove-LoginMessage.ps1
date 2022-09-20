35711f4cc4b35b72242ee3b8de8637941741f925
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 20:32:05 +0200

    Update Remove-LoginMessage.ps1

--- VSA/Agent Procedures/CodeRefactoring/Set Login Message/Remove-LoginMessage.ps1
+++ VSA/Agent Procedures/CodeRefactoring/Set Login Message/Remove-LoginMessage.ps1
@@ -1,4 +1,21 @@
-#Registry path to Windows Update settings
+<#
+=================================================================================
+Script Name:        Management: Remove Login Message.
+Description:        Remove Login Message.
+Lastest version:    2022-04-11
+=================================================================================
+
+
+
+Required variable inputs:
+None
+
+
+
+Required variable outputs:
+None
+#>
+
 $Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\"
 
 Remove-ItemProperty -Path $Path -Name "legalnoticecaption" -ErrorAction SilentlyContinue

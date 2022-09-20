d84bc7a5dbfa4bb8538a9cfbabae17a186d372e5
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-09-20 17:14:56 +0200

    Create Disable-WindowsFirewall.ps1

---
+++ VSA/Agent Procedures/CodeRefactoring/Disable Windows Firewall/Disable-WindowsFirewall.ps1
@@ -0,0 +1,21 @@
+﻿<#
+=================================================================================
+Script Name:        Management: Disable Windows Firewall.
+Description:        This script silently disables firewall on Windows machines.
+Lastest version:    2022-09-20
+=================================================================================
+
+
+
+Required variable inputs:
+None
+
+
+
+Required variable outputs:
+None
+#>
+#Disable Windows Firewall
+Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
+Write-Host "Windows Firewall has been disabled"
+eventcreate /L Application /T INFORMATION /SO VSAX /ID 200 /D "Windows Firewall has been disabled" | Out-Null

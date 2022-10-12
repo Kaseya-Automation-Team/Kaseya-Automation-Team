f27973c78df3ac9a1213c0db212e48ed546dc4d1
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-10-12 14:20:28 +0200

    Update Create-SystemRestorePoint.ps1

--- VSA/Agent Procedures/Backup & Restore Registry/Create-SystemRestorePoint.ps1
+++ VSA/Agent Procedures/Backup & Restore Registry/Create-SystemRestorePoint.ps1
@@ -1,4 +1,13 @@
-﻿$ExistingRestorePoints = Get-ComputerRestorePoint
+﻿<#
+.Synopsis
+   Creates a System Restore Point.
+.DESCRIPTION
+   Creates a System Restore Point. If a Restore Point was created within the last 24 hours, removes it and creates a new one
+.NOTES
+   Version 0.1
+   Author: Proserv Team - VS
+#>
+$ExistingRestorePoints = Get-ComputerRestorePoint
 if ($null -eq $ExistingRestorePoints) {
     Enable-ComputerRestore -Drive "$env:SystemDrive\"
 } else {
@@ -6,4 +15,6 @@ if ($null -eq $ExistingRestorePoints) {
     $dayAgo = [system.management.ManagementDateTimeConverter]::ToDmtfDateTime((Get-Date).AddHours(-24))
     Get-WmiObject -query "SELECT * from Win32_ShadowCopy where Persistent = 'True' AND InstallDate > '$dayAgo'" | ForEach-Object {$_.Delete();}
 }
-Checkpoint-Computer -Description "RegistrySavePoint.$((Get-Date).tostring("MM-dd-yyyy"))" -RestorePointType MODIFY_SETTINGS
+[string]$Description = "RegistrySavePoint.$((Get-Date).tostring("MM-dd-yyyy"))"
+Checkpoint-Computer -Description $Description -RestorePointType MODIFY_SETTINGS
+"Restore Point $Description created" | Write-Output

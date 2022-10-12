d009113947b8dd7889150070b5a5e20e89fb65a5
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-10-12 13:33:32 +0200

    Create Restore-LastRestorePoint.ps1

---
+++ VSA/Agent Procedures/Backup & Restore Registry/Restore-LastRestorePoint.ps1
@@ -0,0 +1,7 @@
+﻿$LastRestorePoint = Get-ComputerRestorePoint | Sort-Object -Property CreationTime -Descending | Select-Object -First 1 | Select-Object -ExpandProperty SequenceNumber
+if ($null -eq $LastRestorePoint) {
+    "No Restore Points Found!" | Write-Output
+} else {
+    Restore-Computer -RestorePoint $LastRestorePoint
+    "The Last Restore Point Reverted" | Write-Output
+}

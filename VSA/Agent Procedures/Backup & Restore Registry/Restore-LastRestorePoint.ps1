93dbf95d6a0e2d952742a023100b3f3d0372f01d
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-10-12 14:20:47 +0200

    Update Restore-LastRestorePoint.ps1

--- VSA/Agent Procedures/Backup & Restore Registry/Restore-LastRestorePoint.ps1
+++ VSA/Agent Procedures/Backup & Restore Registry/Restore-LastRestorePoint.ps1
@@ -1,4 +1,13 @@
-﻿$LastRestorePoint = Get-ComputerRestorePoint | Sort-Object -Property CreationTime -Descending | Select-Object -First 1 | Select-Object -ExpandProperty SequenceNumber
+﻿<#
+.Synopsis
+   Reverts to the Last System Restore Point.
+.DESCRIPTION
+   Reverts to the Last System Restore Point.
+.NOTES
+   Version 0.1
+   Author: Proserv Team - VS
+#>
+$LastRestorePoint = Get-ComputerRestorePoint | Sort-Object -Property CreationTime -Descending | Select-Object -First 1 | Select-Object -ExpandProperty SequenceNumber
 if ($null -eq $LastRestorePoint) {
     "No Restore Points Found!" | Write-Output
 } else {

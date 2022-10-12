612fde483c5d8d58e31b6cc65431b783c484ad4b
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2022-10-12 13:33:26 +0200

    Create Create-SystemRestorePoint.ps1

---
+++ VSA/Agent Procedures/Backup & Restore Registry/Create-SystemRestorePoint.ps1
@@ -0,0 +1,9 @@
+﻿$ExistingRestorePoints = Get-ComputerRestorePoint
+if ($null -eq $ExistingRestorePoints) {
+    Enable-ComputerRestore -Drive "$env:SystemDrive\"
+} else {
+    #remove restore points created in the last 24 hours
+    $dayAgo = [system.management.ManagementDateTimeConverter]::ToDmtfDateTime((Get-Date).AddHours(-24))
+    Get-WmiObject -query "SELECT * from Win32_ShadowCopy where Persistent = 'True' AND InstallDate > '$dayAgo'" | ForEach-Object {$_.Delete();}
+}
+Checkpoint-Computer -Description "RegistrySavePoint.$((Get-Date).tostring("MM-dd-yyyy"))" -RestorePointType MODIFY_SETTINGS

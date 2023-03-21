f11cc65ddb6398bba46e6e16014e77ab5fad5866
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2023-03-21 15:55:51 +0100

    Update Create-SystemRestorePoint.ps1
    Restore Points amount limited to 10.

--- VSA/Agent Procedures/Backup & Restore Registry/Create-SystemRestorePoint.ps1
+++ VSA/Agent Procedures/Backup & Restore Registry/Create-SystemRestorePoint.ps1
@@ -2,18 +2,25 @@
 .Synopsis
    Creates a System Restore Point.
 .DESCRIPTION
-   Creates a System Restore Point. If a Restore Point was created within the last 24 hours, removes it and creates a new one
+   Creates a System Restore Point. If a there are more Restore Points than specified by the Limit, removes older Restore Points and creates a new one
 .NOTES
-   Version 0.1
+   Version 0.2
    Author: Proserv Team - VS
 #>
-$ExistingRestorePoints = Get-ComputerRestorePoint
+param (
+    [parameter(Mandatory=$false)]
+    [int]$Limit = 10
+ )
+
+[array]$ExistingRestorePoints = Get-ComputerRestorePoint
 if ($null -eq $ExistingRestorePoints) {
     Enable-ComputerRestore -Drive "$env:SystemDrive\"
 } else {
-    #remove restore points created in the last 24 hours
-    $dayAgo = [system.management.ManagementDateTimeConverter]::ToDmtfDateTime((Get-Date).AddHours(-24))
-    Get-WmiObject -query "SELECT * from Win32_ShadowCopy where Persistent = 'True' AND InstallDate > '$dayAgo'" | ForEach-Object {$_.Delete();}
+    $TotalRPs = $ExistingRestorePoints.Count
+    if ($TotalRPs -ge $Limit) {
+        [array]$IDToRemove = Get-WmiObject -query "SELECT ID, InstallDate FROM Win32_ShadowCopy WHERE Persistent = 'True'" | Select-Object -Property ID, InstallDate, @{Name="Date"; Expression={$_.ConvertToDateTime($_.InstallDate)}} | Sort-Object -Descending -Property Date | Select-Object -ExpandProperty ID -Last $($TotalRPs - $Limit + 1)
+        Get-WmiObject -query "SELECT ID, InstallDate FROM Win32_ShadowCopy WHERE Persistent = 'True'" | Where-Object { $IDToRemove.Contains( $_.ID ) } | ForEach-Object {$_.Delete();}
+    }
 }
 [string]$Description = "RegistrySavePoint.$((Get-Date).tostring("MM-dd-yyyy"))"
 Checkpoint-Computer -Description $Description -RestorePointType MODIFY_SETTINGS

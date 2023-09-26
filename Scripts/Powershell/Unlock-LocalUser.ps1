2793a313cff8b585fdf6fa699eb43c193ff39d94
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2023-09-26 15:27:22 +0200

    Update Unlock-LocalUser.ps1

--- Scripts/Powershell/Unlock-LocalUser.ps1
+++ Scripts/Powershell/Unlock-LocalUser.ps1
@@ -1,10 +1,32 @@
-﻿param (
+﻿<#
+.Synopsis
+    Unlocks local user account.
+.DESCRIPTION
+    Unlocks local user accounts by resetting passwords or disabling lockouts.
+.PARAMETER UserName
+    The user name to unlock.
+.PARAMETER ResetPassword
+    (Optional) unlock account by resetting password.
+.PARAMETER NewPassword
+    A new password. Must meet length and complexety requirements
+.EXAMPLE
+    .\Unlock-LocalUser -UserName myuser
+.EXAMPLE
+    .\Unlock-LocalUser -UserName myuser -ResetPassword NewP@ssword
+.NOTES
+    Version 0.1
+    Requires:
+        Proper permissions to manage local accounts and execute the script.
+   
+    Author: Proserv Team - VS
+#>
+param (
 [parameter(Mandatory=$true, 
         ParameterSetName = 'Reset')]
 [parameter(Mandatory=$true, 
         ParameterSetName = 'Unlock')]
 [ValidateNotNullOrEmpty()]
-    [string]$Username,
+    [string]$UserName,
 
 [parameter(Mandatory=$true, 
         ParameterSetName = 'Reset')]
@@ -16,7 +38,7 @@
     [string]$NewPassword
 )
 
-$wmiQuery = "SELECT * FROM Win32_UserAccount WHERE Name = '$Username'"
+$wmiQuery = "SELECT * FROM Win32_UserAccount WHERE Name = '$UserName'"
 [array]$users = Get-WmiObject -Query $wmiQuery -Namespace "root\cimv2" | Where-Object { $_.LocalAccount -eq $True -and $_.Lockout -eq $True }
 
 if (0 -lt $users.Count) {

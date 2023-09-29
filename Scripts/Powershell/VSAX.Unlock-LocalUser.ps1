096b1a02dca3cd0edefe0bf4d3b98eaf0e18d2e3
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2023-09-29 13:28:52 +0200

    Create VSAX.Unlock-LocalUser.ps1

---
+++ Scripts/Powershell/VSAX.Unlock-LocalUser.ps1
@@ -0,0 +1,20 @@
+﻿# Inputs
+$UserName = " "
+$NewPassword = " "
+
+$UserName = $UserName.Trim()
+$NewPassword = $NewPassword.Trim()
+
+$wmiQuery = "SELECT * FROM Win32_UserAccount WHERE Name = '$UserName'"
+[array]$users = Get-WmiObject -Query $wmiQuery -Namespace "root\cimv2" | Where-Object { $_.LocalAccount -eq $True -and $_.Lockout -eq $True }
+
+if (0 -lt $users.Count) {
+    foreach ($user in $users) {
+        $user.Lockout = $false
+        $user.Put()
+
+        if ( [string]::IsNullOrEmpty( $NewPassword ) ) {
+            ([adsi]("WinNT://"+$user.caption).replace("\","/")).SetPassword($NewPassword)
+        }
+    }
+}

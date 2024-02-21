08673e5e1d004a6560785fee52972ff17fdceac8
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2024-02-21 21:00:40 +0100

    Update Copy-VSAMGStructure.ps1
    Greatly simplified. Recursion removed

--- VSAModule/public/Copy-VSAMGStructure.ps1
+++ VSAModule/public/Copy-VSAMGStructure.ps1
@@ -45,6 +45,10 @@
         [string] $OrgRef
     )
 
+    if ( $SourceVSA -eq $DestinationVSA ) {
+        throw "The Source and the Destionation is the same VSA Environment!"
+    }
+
     [hashtable]$SourceParams = @{
         VSAConnection = $SourceVSA
         Filter = "OrgRef eq '$OrgRef'"
@@ -81,146 +85,104 @@
 
     [array]$global:DestinationMGs = Get-VSAMachineGroup @DestinationParams
 
-    Foreach ($MachineGroup in $($SourceMGs | Sort-Object -Property @{Expression = { $_.MachineGroupName.Split('.').Count }}, @{Expression = {$_.ParentMachineGroupId}; Ascending = $false} , @{Expression = {$_.MachineGroupName}; Ascending = $false}) )
-    {
+    Foreach ($MachineGroup in $SourceMGs | Sort-Object -Property @{
+                    Expression = { $_.MachineGroupName.Split('.').Count }
+                    }, @{
+                    Expression = {$_.MachineGroupName}; Ascending = $false} ) {
+
         [hashtable]$DestinationParams = @{
             VSAConnection = $DestinationVSA
             OrgID         = $DestinationOrg.OrgId
         }
         
-        #$SplitName = ($MachineGroup.MachineGroupName | Select-String -Pattern "(?:\.root).*$").Matches.Value
-        #$SplitName = ($MachineGroup.MachineGroupName | Select-String -Pattern "(?<=$OrgRef).*$").Matches.Value
-        $global:DestinationMGs = Get-VSAMachineGroup @DestinationParams
-        [array]$CheckDestination = $global:DestinationMGs | Where-Object { $_.MachineGroupName -eq $MachineGroup.MachineGroupName }
+        #region Define the current Machine Group's own MachineGroupName and the parent Machine Group's MachineGroupName (if exists)
+        #$SplitName = ($MachineGroup.MachineGroupName | Select-String -Pattern "(?<=$OrgRef\.).*$").Matches.Value
 
-        #region message
-        $Info = "Look up Destination for Machine Group '$($MachineGroup.MachineGroupName)'. Found: [$($CheckDestination.Count)] object(s)."
-        Write-Host $Info
-        if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) { Write-Debug $Info }
-        if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) { Write-Verbose $Info }
-        #endregion message
-
-        $NewMGObj = $MachineGroup
-        
-        #region Check if there's Parent Machine Group and the Parent Machine Group already exists in the destination.
-        if( -not [string]::IsNullOrEmpty( $MachineGroup.ParentMachineGroupId ) ) {
-            #region message
-            if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) {
-                Write-Debug "Looking for parent of the Machine Group of '$($MachineGroup.MachineGroupName)' in [$($global:ExistingSourceMGs.MachineGroupName -join '; ')]"
-            }
-            if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
-                Write-Verbose "Looking for parent of the Machine Group of '$($MachineGroup.MachineGroupName)'"
-            }
-            #endregion message
-            #obtain ParentMachineGroupId by MachineGroupName
-            $ParentMachineGroupName = $global:ExistingSourceMGs | Where-Object { $_.MachineGroupId -eq $MachineGroup.ParentMachineGroupId } | Select-Object -ExpandProperty MachineGroupName
-            if ( [string]::IsNullOrEmpty($ParentMachineGroupName) ) {
-                #region message
-                if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) {
-                    Write-Debug "Parent Machine Group for '$($MachineGroup.MachineGroupName)' not found in the source. Skip"
-                }
-                if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
-                    Write-Verbose "Parent Machine Group for '$($MachineGroup.MachineGroupName)' not found in the source. Skip"
-                }
-                #endregion message
-                continue
-            } else {
-                $ParentMachineGroupId = $global:DestinationMGs | Where-Object { $_.MachineGroupName -eq $ParentMachineGroupName } | Select-Object -ExpandProperty MachineGroupId
-
-                #region message
-                if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) {
-                    Write-Debug "Parent Machine Group for '$($MachineGroup.MachineGroupName)' is '$ParentMachineGroupName'. Destination ParentMachineGroupId is: '$ParentMachineGroupId'."
-                }
-                if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
-                    Write-Verbose "Parent Machine Group for '$($MachineGroup.MachineGroupName)' is '$ParentMachineGroupName'. Destination ParentMachineGroupId is: '$ParentMachineGroupId'."
-                }
-                #endregion message
-
-                $NewMGObj.ParentMachineGroupId = $ParentMachineGroupId 
+        [string] $MGName = $MachineGroup.MachineGroupName
+        [string[]] $MGNameAsArray =  $MGName.split('.')
+        [string] $OwnMGName = $MGNameAsArray[-1]
+        $ParentMGName = [string]::Empty
+        if ($MGNameAsArray.Count -gt 2) {
+            [int] $strLength =  $MGName.LastIndexOf('.')
+            if (0 -lt $strLength ) {
+                [string] $ParentMGName =  $MGName.Substring( 0, $strLength )
             }
         }
-        #endregion Check if there's Parent Machine Group and the Parent Machine Group already exists in the destination.
+
+        $MachineGroup.MachineGroupName = $OwnMGName
+        #endregion Define the current Machine Group's own MachineGroupName and the parent Machine Group's MachineGroupName (if exists)
+
+        # Check if the Parent Organization already exists in the destination.
+        if ( -not [string]::IsNullOrEmpty($ParentMGName) ) {
+
+            $DestinationParentMGId =  Get-VSAMachineGroup @DestinationParams -Filter "MachineGroupName eq '$ParentMGName'" | Select-Object -ExpandProperty MachineGroupId
+
+            #region message
+            if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) {
+                Write-Debug "Parent Machine Group for '$MGName' : '$ParentMGName'. Destination ParentMachineGroupId: '$DestinationParentMGId'."
+            }
+            if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
+                Write-Debug "Parent Machine Group for '$MGName' : '$ParentMGName'. Destination ParentMachineGroupId: '$DestinationParentMGId'."
+            }
+            #endregion message
+            
+            #Replace source's ParentMachineGroupId with the destination's counterpart
+            $MachineGroup.ParentMachineGroupId = $DestinationParentMGId
+        }
+
+        $CheckDestination = Get-VSAMachineGroup @DestinationParams -Filter "MachineGroupName eq '$MGName'"
     
-        if( 0 -eq $CheckDestination.Count) #No MG with this name in the destination
+        if( $null -eq $CheckDestination ) #No MG with this name in the destination
         {
             $NewMGParams = $DestinationParams.Clone()
             $NewMGParams.Add('ExtendedOutput', $true)
 
-            if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) { $NewMGParams.Add('Debug', $true) }
+            if ($PSCmdlet.MyInvocation.BoundParameters['Debug'])   { $NewMGParams.Add('Debug', $true) }
             if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) { $NewMGParams.Add('Verbose', $true) }
 
-            #New MG Params
-            $NameToCreate = ($($MachineGroup.MachineGroupName).split('.'))[-1]
-            $NewMGObj.MachineGroupName = $NameToCreate
-
             #region message
             if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) {
-                Write-Debug "Machine Group '$($MachineGroup.MachineGroupName)' not found in the destination. Attempting to create it."
+                Write-Debug "Machine Group with name '$MGName' was not found in the destination. Will attempt to create it."
             }
             if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
-                Write-Verbose "Machine Group '$($MachineGroup.MachineGroupName)' not found in the destination. Attempting to create it."
+                Write-Debug "Machine Group with name '$MGName' was not found in the destination. Will attempt to create it."
             }
             #endregion message
 
-            $GroupId = $NewMGObj | New-VSAMachineGroup @NewMGParams
-            $global:DestinationMGs = Get-VSAMachineGroup @DestinationParams
-        } else  { # A MG with this name already exists in the destination
-            $GroupId  = $CheckDestination.MachineGroupId
+            $NewGroupId = try {
+                $MachineGroup | New-VSAMachineGroup @NewMGParams
+            } catch {
+                $_.Exception.Message
+            }
+
+            if ($NewGroupId -match "^\d+$") {
+                #region message
+                $Info = "Successfully created Machine Group '$MGName' with ID '$NewGroupId'."
+                Write-Host $Info
+                if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) { Write-Verbose $Info }
+                if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) { Write-Debug $Info }
+                #endregion message
+
+                #Make the REST API wait while the BackEnd updates the information
+                $GetMGParams = $DestinationParams.Clone()
+                $GetMGParams.Remove('OrgId')
+                $GetMGParams.Add('MachineGroupId', $NewGroupId)
+                $null = Get-VSAMachineGroup @GetMGParams
+            } else {
+                #region message
+                $Info = "Something went wrong while creating '$MGName'. Returned ID: '$NewGroupId'"
+                Write-Host $Info -ForegroundColor Red
+                if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) { Write-Verbose $Info }
+                if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) {
+                    Write-Debug $Info
+                    Write-Debug "JSON data:`n$($MachineGroup | ConvertTo-Json -Depth 3 | Out-String)"
+                }
+                #endregion message
+            } 
         }
-        #region message
-        $Info = "GroupId of the destination Machine Group '$($MachineGroup.MachineGroupName)' is: '$GroupId'"
-        $Info | Write-Host
-        if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) { Write-Debug $Info }
-        if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) { Write-Verbose $Info }
-        #endregion message
-
-        [array]$DirectChildren = $global:ExistingSourceMGs | Where-Object {$_.ParentMachineGroupId -eq $MachineGroup.MachineGroupId }
-
-        #region message
-        $Info = "Amount of Direct Children of Machine Group '$($MachineGroup.MachineGroupName)' is [$($DirectChildren.Count)]"
-        $Info | Write-Host
-        if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) { Write-Verbose $Info }
-        if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) {
-            Write-Debug $Info
-            $DirectChildren | Select-Object -ExpandProperty MachineGroupName | Out-String | Write-Debug
-        }
-        #endregion message
-
-        if ( 0 -lt $DirectChildren.Count)
-        {
-            #region message
-            if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) {
-                Write-Debug "Direct children of <$($MachineGroup.MachineGroupName)> : [$(($DirectChildren | Select-Object -ExpandProperty MachineGroupName) -join '; ')]"
-            }
-            if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
-                Write-Verbose "Direct children of <$($MachineGroup.MachineGroupName)> : [$(($DirectChildren | Select-Object -ExpandProperty MachineGroupName) -join '; ')]"
-            }
-            #endregion message
-
-            [hashtable]$CreateMGParams = @{
-                OrgRef = $OrgRef
-                SourceVSA = $SourceVSA
-                DestinationVSA = $DestinationVSA
-                SourceMGs = $DirectChildren
-            }
-
-            if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) { $CreateMGParams.Add('Debug', $true) }
-            if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) { $CreateMGParams.Add('Verbose', $true) }
-
-            #region message
-            $Info = "--- Processing Direct children of the '$($MachineGroup.MachineGroupName)' Machine Group ---`n[$($DirectChildren.Count)] Direct children of '$($MachineGroup.MachineGroupName)' to be created:`n[$($DirectChildren.MachineGroupName -join '; ')]`n==============="
-            Write-Host $Info
-            if ($PSCmdlet.MyInvocation.BoundParameters['Debug']) { 
-                $Info += "`nRecursive call of Copy-VSAMGStructure with parameters:`n'$($CreateMGParams | Out-String)'"
-                Write-Debug $Info
-            }
-            if ($PSCmdlet.MyInvocation.BoundParameters['Verbose']) {
-                $Info += "`nRecursive call of Copy-VSAMGStructure"
-                Write-Verbose $Info
-            }
-            #endregion message
-
-            Copy-VSAMachineGroupStructure @CreateMGParams
+        else {
+            #$CheckDestination is not null
+            continue
         }
 
     } # Foreach ($MachineGroup in $SourceMGs)

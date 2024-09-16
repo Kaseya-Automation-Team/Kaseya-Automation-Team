e60f067b39324d2b72e58bee400ee2babbc9f79f
vladislav-semko-kaseya <73660905+vladislav-semko-kaseya@users.noreply.github.com>
2024-09-16 10:41:27 +0200

    Create Get-VSASDTicketCustomField.ps1

---
+++ VSAModule/public/Get-VSASDTicketCustomField.ps1
@@ -0,0 +1,92 @@
+function Get-VSASDTicketCustomField {
+    <#
+    .Synopsis
+       Returns the value of a custom field in a service desk ticket.
+    .DESCRIPTION
+       Returns the value of a custom field in a service desk ticket.
+       Takes either persistent or non-persistent connection information.
+    .PARAMETER VSAConnection
+        Specifies existing non-persistent VSAConnection.
+    .PARAMETER URISuffix
+        Specifies URI suffix if it differs from the default.
+    .PARAMETER ServiceDeskId
+        Specifies id of service desk
+    .PARAMETER Filter
+        Specifies REST API Filter.
+    .PARAMETER Paging
+        Specifies REST API Paging.
+    .PARAMETER Sort
+        Specifies REST API Sorting.
+    .EXAMPLE
+       Get-VSASDTicketCustomField -ServiceDeskTicketId 123456 -CustomFieldId 654321
+    .EXAMPLE
+       Get-VSASDTicketCustomField -VSAConnection $connection -ServiceDeskTicketId 1233456 -CustomFieldId 654321
+    .INPUTS
+       Accepts piped non-persistent VSAConnection 
+    .OUTPUTS
+       Array of items that represent custom fields of service desk ticket
+    #>
+
+    [CmdletBinding()]
+    param ( 
+        [parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true)]
+        [VSAConnection] $VSAConnection,
+
+        [parameter(DontShow, Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $URISuffix = 'api/v1.0/automation/servicedesktickets/{0}/customfields/{1}',
+
+        [parameter(Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true)]
+        [ValidateScript({
+            if( (-not [string]::IsNullOrEmpty($_)) -and ($_ -notmatch "^\d+$") ) {
+                throw "Non-numeric Id"
+            }
+            return $true
+        })]
+        [string] $ServiceDeskTicketId,
+
+        [parameter(Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true)]
+        [ValidateScript({
+            if( (-not [string]::IsNullOrEmpty($_)) -and ($_ -notmatch "^\d+$") ) {
+                throw "Non-numeric Id"
+            }
+            return $true
+        })]
+        [string] $CustomFieldId,
+
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Filter,
+
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Paging,
+
+        [parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $Sort
+    )
+
+    [hashtable]$Params = @{
+        URISuffix     = $( $URISuffix -f $ServiceDeskTicketId, $CustomFieldId )
+        VSAConnection = $VSAConnection
+        Filter        = $Filter
+        Paging        = $Paging
+        Sort          = $Sort
+    }
+
+    foreach ( $key in $Params.Keys.Clone()  ) {
+        if ( -not $Params[$key]) { $Params.Remove($key) }
+    }
+
+    return Invoke-VSARestMethod @Params
+}
+New-Alias -Name Get-VSASDTicketCustomFields -Value Get-VSASDTicketCustomField
+Export-ModuleMember -Function Get-VSASDTicketCustomField -Alias Get-VSASDTicketCustomFields

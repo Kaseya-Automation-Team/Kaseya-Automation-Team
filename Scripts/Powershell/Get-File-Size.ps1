234a211bbfafc7ba18264606cf47362091c3168b
vladislav-semko-kaseya <vladislav.semko@kaseya.com>
2026-03-27 15:03:28 +0100

    Moved To Private

--- Scripts/Powershell/Get-File-Size.ps1
+++
@@ -1,7 +0,0 @@
-﻿$folder = $args[0]
-$sz = $args[1]
-if ($sz.ToLower() -eq "gb") {
-    "{0}" -f [math]::Round(((Get-ChildItem $folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1GB),2)
-} else {
-	"{0}" -f [math]::Round(((Get-ChildItem $folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB),2)
-}
